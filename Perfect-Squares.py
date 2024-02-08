b13f15efa17583791edf663a25f330bbf739e077
amilbcahat <68186313+amilbcahat@users.noreply.github.com>
2024-02-09 01:14:08 +0530

    Perfect Squares 9 Feb 2024

--- Perfect-Squares.py
+++ Perfect-Squares.py
@@ -1,8 +1,22 @@
 
+# Given an integer n, return the least number of perfect square numbers that sum to n.
 
+# A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 
+ 
 
+# Example 1:
 
+# Input: n = 12
+# Output: 3
+# Explanation: 12 = 4 + 4 + 4.
+# Example 2:
+
+# Input: n = 13
+# Output: 2
+# Explanation: 13 = 4 + 9.
+ 
+#Solution 1 => n * sqrt(n)
 class Solution:
     def numSquares(self, n: int) -> int:
         #Same logic as Coin change problem ! just evaluate the coins here !
@@ -19,3 +33,22 @@ class Solution:
                     dp[a] = min(dp[a] , 1 + dp[a - c]) 
         
         return dp[n] if dp[n] != float('inf') else -1  
+
+#Solution 2 ->  n * sqrt(n)
+#Same logic as Coin change problem ! just evaluate the coins here !
+        dp = [float("inf")] * (n + 1)
+        #To get zero coins , we need 0 coins 
+        dp[0] = 0 
+        #Another extra test case , since we can get 1 (perfect sum) to get 1 amounted sum 
+        dp[1] = 1
+
+        #Little bit enchanced method !
+        for a in range(1 , n+1):
+            c = 1 
+            while c * c <= a : 
+                #We look for squares till that amount 
+                dp[a] = min(dp[a] , 1 + dp[a - (c * c)]) 
+                c += 1    
+        return dp[n] if dp[n] != float('inf') else -1  
+
+#Solution 3 -> Lagrange 3 and 4 Theorem 
