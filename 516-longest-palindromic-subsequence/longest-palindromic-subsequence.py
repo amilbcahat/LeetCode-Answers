128cd2d65ff816d4eb7c75c6cc8706bcbd6b6e1b
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-02-11 20:24:58 +0530

    Time: 1366 ms (39.56%) | Memory: 33 MB (77.60%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,19 +1,47 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-
-        #Reverse and find the LCS between them , since Pali are same , even after reversing , their longest common part would be the 
-        #Longest Common Subsequence !
-        s1 = s 
-        s2 = s[::-1]
-
-        N , M = len(s1) , len(s2)
-        dp = [[0] * (M + 1) for _ in range(N + 1)]
-
-        for i in range(N):
-            for j in range(M):
-                if s1[i] == s2[j]:
-                    dp[i + 1][j + 1] = 1 + dp[i][j]
+        #O(n ** 2)
+        #Bottom -Up Approach 
+        dp= [[0] * (len(s) + 1) for _ in range(len(s) + 1)]
+        res = 0 
+        #right to left from bottom
+        for i in range(len(s)):
+            #Traverse till current item
+            for j in range(len(s) - 1, i - 1 , -1):
+                if s[i] == s[j]:
+                    dp[i][j] = 1 if i == j else 2 
+                    if i - 1 >= 0 : 
+                        dp[i][j] += dp[i - 1][j + 1]
                 else:
-                    dp[i + 1][j + 1] = max(dp[i][j + 1] , dp[i + 1][j])
+                    dp[i][j] = dp[i ][j + 1]
+                    if i - 1 >= 0 : 
+                        dp[i][j] = max(dp[i][j + 1] , dp[i - 1][j])
+                res = max(res , dp[i][j])
+        return res 
 
-        return dp[N][M] 
+        #Top Down Approach 
+        # cache = {}
+        # #Much of the logic is borrowed from LCS 
+        # def dfs(i , j):
+        #     if i < 0 or j == len(s):
+        #         return 0 
+
+        #     if (i , j) in cache : 
+        #         return cache[(i , j)]
+
+        #     if s[i] == s[j]:
+        #         length = 1 if i == j else 2 
+        #         #When it is a Pali , keep on search for more length Pali
+        #         cache[(i, j)] = length + dfs(i - 1 , j + 1)
+        #     else:
+        #         #if some s[l] != s[r] , then just skip from left or right , and keep on searching for better alternatives 
+        #         #for longest answer 
+        #         cache[(i , j)] = max(dfs(i , j + 1) , dfs(i - 1, j))
+
+        #     return cache[(i , j)]
+        
+        # for i in range(len(s)):
+        #     dfs(i , i),  #Odd length Pali  
+        #     dfs(i , i + 1) #Even Length Pali
+
+        # return max(cache.values())
