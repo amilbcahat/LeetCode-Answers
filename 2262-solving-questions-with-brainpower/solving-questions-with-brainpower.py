00df10b959e79dbb7fa30b849380fae6965f65a6
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-07 11:19:03 +0530

    Time: 1270 ms (62.85%) | Memory: 71.1 MB (44.97%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,23 +1,34 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        
-        cache = {}
-        def dfs(i):
-            if i >= len(questions):
-                return 0 
+        #Bottom-Up Approach
+        dp = {}
 
-            if i in cache : 
-                return cache[i]
+        for i in range(len(questions) - 1 , -1 , -1):
+            dp[i] = max(
+                questions[i][0] + dp.get(i + 1 + questions[i][1] , 0),
+                dp.get(i + 1 , 0)
+            )
 
-            #Take Question#
+        return dp[0]
+
+        #Top Down Approach
+        # cache = {}
+        # def dfs(i):
+        #     if i >= len(questions):
+        #         return 0 
+
+        #     if i in cache : 
+        #         return cache[i]
+
+        #     #Take Question#
             
-            take = questions[i][0] + dfs(i + (questions[i][1] + 1))
+        #     take = questions[i][0] + dfs(i + (questions[i][1] + 1))
 
-            #Dont take Question 
-            skip = dfs(i + 1)
+        #     #Dont take Question 
+        #     skip = dfs(i + 1)
 
-            cache[i] = max(take , skip)
+        #     cache[i] = max(take , skip)
 
-            return cache[i]
+        #     return cache[i]
 
-        return dfs(0)
+        # return dfs(0)
