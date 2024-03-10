9e4cbd7cf58fa80eb8643fc493d57a8d0a6b3b9c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-10 07:51:58 +0530

    Time: 93 ms (98.09%) | Memory: 16.6 MB (94.27%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.py
+++ 1105-uncrossed-lines/uncrossed-lines.py
@@ -1,25 +1,52 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        #Memory Optimized DP Solution
+        prev = [0] * (len(nums2) + 1)
+
+        for i in range(len(nums1)):
+            dp = [0] * (len(nums2) + 1)
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    dp[j + 1] = 1 + prev[j]
+                else:
+                    dp[j + 1] = max(
+                        dp[j],
+                        prev[j + 1]
+                    )
+            prev = dp
+
+        return prev[-1]
+        #Pure DP 
+        # dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i] == nums2[j] : 
+        #             dp[i + 1][j + 1] = 1 + dp[i][j]
+        #         else:
+        #             dp[i + 1][j + 1] = max(
+        #                 dp[i][j + 1], #top
+        #                 dp[i + 1][j] #left
+        #             )
+
+        # return dp[len(nums1)][len(nums2)]
 
         #Variation of Longest Common Subsequence#
-        cache = {}        
-        def dfs(i , j):
-            if i >= len(nums1) or j >= len(nums2):
-                return 0 
+        # cache = {}        Space - O(n * m) TC - 2^(n + m)
+        # def dfs(i , j):
+        #     if i >= len(nums1) or j >= len(nums2):
+        #         return 0 
 
-            if (i , j) in cache : 
-                return cache[(i , j)]
+        #     if (i , j) in cache : 
+        #         return cache[(i , j)]
 
-            res1 = float("-inf")
+        #     res1 = float("-inf")
 
-            if nums1[i] == nums2[j]:
-                res1 = 1 + dfs(i + 1 , j + 1)
+        #     if nums1[i] == nums2[j]:
+        #         cache[(i , j)] = 1 + dfs(i + 1 , j + 1)
+        #     else:
+        #         cache[(i , j)] = max(dfs(i + 1 , j) , dfs(i , j + 1))
 
-            res2 = dfs(i + 1 , j)
-            res3 = dfs(i , j + 1)
+        #     return cache[(i , j)] 
 
-            cache[(i , j)] = max(res1 , res2 , res3)
-
-            return cache[(i , j)] 
-
-        return dfs(0 , 0)
+        # return dfs(0 , 0)
