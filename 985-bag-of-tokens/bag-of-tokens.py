98b2ca756894d38cbb24f79dee8779660bb6e9e9
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-18 15:45:50 +0530

    Time: 65 ms (5.10%) | Memory: 16.8 MB (12.71%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -0,0 +1,30 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        print(tokens)
+        l ,r = 0 ,len(tokens) - 1
+        score = 0 
+        maxScore = 0
+        #Increment left pointer to increment and maximize the scores 
+        #Decrement with right pointer , to survive and not let the conditions die !
+        while l <= r : 
+            while l <= r and power >= tokens[l]:
+                power = power - tokens[l]
+                l += 1
+                score += 1 
+                maxScore = max(score ,maxScore)
+                print("l" , l , power , score)
+            
+            while l <= r and power < tokens[l]:
+                power = power + tokens[r]
+                r -= 1 
+                score -= 1 
+                maxScore = max(score ,maxScore)
+                if score < 0 : 
+                    return maxScore 
+                print("r" , r , power , score)
+
+
+        return maxScore 
+
+        
