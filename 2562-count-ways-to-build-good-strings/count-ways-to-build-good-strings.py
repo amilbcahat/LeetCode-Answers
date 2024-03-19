1a9d7f68338c342129fa77cd51867c356427da0c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-20 04:14:34 +0530

    Time: 141 ms (72.65%) | Memory: 32.8 MB (65.41%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -1,25 +1,37 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        res = 0
+        #Dynamic PRogramming method (less intuitive)-
+        dp = {0 : 1} # length -> num of strs 
         mod = 10 ** 9 + 7 
-        cache = {}
-        #This time a little different 
-        def dfs(resLen):
-            nonlocal res
-            if high < resLen :
-                return 0
 
-            if resLen in cache : 
-                return cache[resLen]
+        for i in range(1 , high + 1):
+            dp[i] = (dp.get(i - one , 0) + dp.get(i - zero , 0)) % mod 
+
+        
+        #counts for all string of length between that range 
+        return sum([dp[i] for i in range(low , high + 1)]) % mod 
+
+
+        # res = 0
+        # mod = 10 ** 9 + 7 
+        # cache = {}
+        # #This time a little different 
+        # def dfs(resLen):
+        #     nonlocal res
+        #     if high < resLen :
+        #         return 0
+
+        #     if resLen in cache : 
+        #         return cache[resLen]
             
-            res = 1 if resLen >= low else 0 
+        #     res = 1 if resLen >= low else 0 
 
-            res += (dfs(resLen + zero)+ dfs(resLen + one))
+        #     res += (dfs(resLen + zero)+ dfs(resLen + one))
 
-            cache[resLen] = res % mod 
+        #     cache[resLen] = res % mod 
             
-            return cache[resLen]  
+        #     return cache[resLen]  
 
             
         
-        return dfs(0)
+        # return dfs(0)
