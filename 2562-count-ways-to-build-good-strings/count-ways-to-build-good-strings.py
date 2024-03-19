e4c319172d7735b137e7bc5c9fbfd178f375c35c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-20 04:18:18 +0530

    Time: 146 ms (68.16%) | Memory: 32.6 MB (65.99%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -1,8 +1,14 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        #One thing is to note that , before it was 2^n backtracking soln , but with dp , we get linear time soln (we dont need to change and 
+        #backtrack on basis of string , so two string can have same length but still different solution , still , we just need 
+        # length of string , so its like , ek teer do nishaane !!)
+
         #Dynamic PRogramming method (less intuitive)-
         dp = {0 : 1} # length -> num of strs 
         mod = 10 ** 9 + 7 
+        #There is no need of low <= x <= high , check , since , 1 - D array has length of max high and starts from low , so its kind of obvious 
+        #That string is built from 
 
         for i in range(1 , high + 1):
             dp[i] = (dp.get(i - one , 0) + dp.get(i - zero , 0)) % mod 
