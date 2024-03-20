5ff4189d903e928b869abff890e5d8cc0c041370
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-20 21:44:19 +0530

    Time: 190 ms (67.68%) | Memory: 21.2 MB (31.60%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
@@ -5,25 +5,44 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        
-        count = 0 
-        first = list1 
-        while first and first.next and count != (a - 1): 
-            first = first.next
-            count += 1 
+        #Neetcode solution
+        curr = list1 
+        i = 0 
+        while i < a - 1: 
+            curr = curr.next 
+            i += 1 
 
-        count = 0 
-        second = list1
-        while second and second.next and count != (b): 
-            second = second.next
-            count += 1 
+        head = curr 
+        while i <= b : 
+            curr = curr.next 
+            i += 1 
+        head.next = list2
 
-        first.next = list2
-
-        p1 = list2 
-        while p1 and p1.next: 
-            p1 = p1.next 
-
-        p1.next = second.next
+        while list2.next : 
+            list2 = list2.next 
+        list2.next = curr
 
         return list1
+
+        #My solution 
+        # count = 0 
+        # first = list1 
+        # while first and first.next and count != (a - 1): 
+        #     first = first.next
+        #     count += 1 
+
+        # count = 0 
+        # second = list1
+        # while second and second.next and count != (b): 
+        #     second = second.next
+        #     count += 1 
+
+        # first.next = list2
+
+        # p1 = list2 
+        # while p1 and p1.next: 
+        #     p1 = p1.next 
+
+        # p1.next = second.next
+
+        # return list1
