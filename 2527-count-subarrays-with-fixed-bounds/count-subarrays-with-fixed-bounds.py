382f67386a3ba3a5b8009288207ab64bc7d32fa8
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-03-31 21:40:57 +0000

    Time: 676 ms (83.56%) | Memory: 30.6 MB (95.26%) - LeetSync

---
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        lastMinK = -1
+        lastMaxK = -1
+        res = 0 
+        culpritIndex = -1
+
+        for i in range(len(nums)):
+            if nums[i] > maxK or nums[i] < minK :
+                #Where existing subarray ends 
+                culpritIndex = i
+
+            if nums[i] == maxK :
+                lastMaxK = i
+            
+            if nums[i] == minK :
+                lastMinK = i
+
+            #start of valid subarray
+            startOfSubArray = min(lastMinK , lastMaxK)
+            #Length of valid subarray
+            temp = startOfSubArray - culpritIndex
+            res += temp if temp > 0 else 0 
+
+        return res 
