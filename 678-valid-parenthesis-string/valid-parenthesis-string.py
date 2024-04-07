6497d05b1064715c1ef69da03cff82ae743f4a07
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-04-07 23:32:17 +0000

    Time: 28 ms (92.98%) | Memory: 16.5 MB (50.39%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        left = []
+        ast = []
+
+        for i,char in enumerate(s):
+            if char == '(':
+                left.append(i)
+            elif char == '*':
+                ast.append(i)
+                # The next three conditions are when char == ")"
+            elif left : # Match left paren with right paren
+                left.pop()
+            elif ast : # Match asterix with right paren
+                ast.pop()
+            else : # Can't match this right paren with anything
+                return False 
+# Found matches for all right parens, now match any leftover left parens
+		# Ensure any asterix used as a right paren comes after the left paren we are matching
+        
+        while left and ast and left[-1] < ast[-1]:
+            left.pop()
+            ast.pop()
+		# If there's any leftover left parens, then we couldn't match those, so False
+        return not left 
