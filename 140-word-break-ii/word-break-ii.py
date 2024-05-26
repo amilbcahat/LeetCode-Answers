b04bfe8be03fe5c7f77267fb28a6d35593555a73
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-05-26 06:36:00 +0530

    Time: 43 ms (6.60%) | Memory: 16.5 MB (43.29%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        
+        wordDict = set(wordDict)
+        cache = {}
+        def backtrack(i):
+            if i == len(s):
+                #Was able to make a string 
+                return [""]
+
+            if i in cache:
+                return cache[i]
+            #Could not make any string 
+            res = []
+            for j in range(i, len(s)):
+                #xxxxx -> xxxx , xxx , xx , x are the subproblem 
+                w = s[i : j + 1]
+                if w not in wordDict: 
+                    continue
+                
+                #Gets some strings in array 
+                strings = backtrack(j + 1)
+                #Add space as required 
+                for substr in strings : 
+                    sentence = w 
+                    if substr : 
+                        sentence += " " + substr
+                    #if no substr then append this string 
+                    res.append(sentence)
+
+            return res 
+
+        return backtrack(0)
