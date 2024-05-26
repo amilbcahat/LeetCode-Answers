d2ba8c49299a4c693465191571690ffcc67f83a4
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-05-26 06:42:25 +0530

    Time: 34 ms (64.14%) | Memory: 16.5 MB (43.29%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -2,6 +2,26 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
         wordDict = set(wordDict)
+        # cur = []
+        # res = []
+
+        # def backtrack(i):
+        #     if i == len(s):
+        #         print(cur)
+        #         res.append(" ".join(cur))
+        #         return 
+
+        #     for j in range(i , len(s)):
+        #         w = s[i : j + 1]
+        #         if w in wordDict : 
+        #             cur.append(w)
+        #             backtrack(j + 1)
+        #             cur.pop()
+
+        # backtrack(0)
+        # return res 
+
+        #Optimal Subproblem solution !
         cache = {}
         def backtrack(i):
             if i == len(s):
@@ -15,6 +35,7 @@ class Solution:
             for j in range(i, len(s)):
                 #xxxxx -> xxxx , xxx , xx , x are the subproblem 
                 w = s[i : j + 1]
+                  #does not matter in this case 
                 if w not in wordDict: 
                     continue
                 
@@ -27,7 +48,7 @@ class Solution:
                         sentence += " " + substr
                     #if no substr then append this string 
                     res.append(sentence)
-
+            cache[i] = res 
             return res 
 
         return backtrack(0)
