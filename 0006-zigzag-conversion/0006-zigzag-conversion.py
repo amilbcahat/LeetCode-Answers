4ee3476aa3a29ebc90cd82445fe238966b036baa
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-05 06:53:23 +0530

    Time: 47 ms (77.99%), Space: 16.6 MB (66.56%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 : 
+            return s 
+
+        ##Sent ###
+        ##
+        ##
+        ##yo#####
+        res = ""
+        for r in range(numRows):
+            increment = 2 * (numRows - 1) 
+            for i in range(r , len(s) , increment):
+                #This gets appended first to string 
+                res += s[i]
+                #mid rows 
+                if (r > 0 and r < numRows - 1 and 
+                (i + increment - 2 * r < len(s))):
+                    res += s[i + increment - 2 * r]
+
+        return res 
