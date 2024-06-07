8abe257d9f0bd1b1a518227f4b0db6f2817ca836
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-07 08:08:53 +0530

    Time: 137 ms (99.81%), Space: 18 MB (28.37%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        #Boyer - Moore Algorithm (Would only work for Majority element case !)
+        #BAsically , we cancel out the frequencies , by going through the array
+        #And get the element with majority 
+        res , count = 0 , 0 
+
+        for n in nums : 
+            #If count of a specific element goes 0 , then change the element as it will introduce 
+            #the possibility of a better ans 
+            if count == 0 :
+                res = n 
+            
+            #Increment if found a match with currently assument ans , else decrement 
+            count += (1 if n == res else -1)
+
+        return res 
