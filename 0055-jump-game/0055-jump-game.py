01895a2893688ed5260d9b072c8e00e9944f9444
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-07 10:51:58 +0530

    Time: 343 ms (83.04%), Space: 17.9 MB (51.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,16 +1,28 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        #Greedy 
+        #Start from last position , shift the goal position , and see with each iteration if its reachable
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1 , -1 , -1):
+            #i + nums[i] is farthest jump distance 
+            if i + nums[i] >= goal : 
+                goal = i 
+
+        return True if goal == 0 else False 
+
+
         #DP
-        dp = [False] * (len(nums))
+        # dp = [False] * (len(nums))
 
-        dp[-1] = True 
+        # dp[-1] = True 
 
-        for i in range(len(nums) - 2 , -1 , -1):
-            furthestJump = min(i + nums[i] , len(nums) - 1)
-            #Can make these length jumps 
-            for j in range(i + 1 , furthestJump + 1):
-                if dp[j] : 
-                    dp[i] = dp[j] 
-                    break
+        # for i in range(len(nums) - 2 , -1 , -1):
+        #     furthestJump = min(i + nums[i] , len(nums) - 1)
+        #     #Can make these length jumps 
+        #     for j in range(i + 1 , furthestJump + 1):
+        #         if dp[j] : 
+        #             dp[i] = dp[j] #Can also write dp[i] = True 
+        #             break
 
-        return dp[0]
+        # return dp[0]
