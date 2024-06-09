9b3ab99064759489f63871d2611124ddb9a59ac7
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-09 19:27:36 +0530

    Time: 39 ms (45.59%), Space: 16.4 MB (97.6%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Assuming arr only contains 0, 1 or 2
+        counts = [0, 0, 0]
+
+        # Count the quantity of each val in arr
+        for n in arr:
+            counts[n] += 1
+        
+        # Fill each bucket in the original array
+        i = 0
+        for n in range(len(counts)):
+            for j in range(counts[n]):
+                arr[i] = n
+                i += 1
+        return arr
