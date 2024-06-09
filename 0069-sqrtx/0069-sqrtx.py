2ca620f8c78934b51588301e1a397c28450e536a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-09 16:36:29 +0530

    Time: 44 ms (37.27%), Space: 16.5 MB (33.64%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,18 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        ##neetcode solution 
         l , r = 1 , x 
-
+        res = 0
         while l <= r : 
             
-            mid = (l + r) // 2
-            target = x // mid 
-
-            print(l ,r , mid , target)
-            if target == mid : 
+            mid = (l + (r - l) // 2)
+            
+            if mid ** 2 == x : 
                 return mid 
-            elif target > mid : 
+            elif mid ** 2 < x : 
                 l = mid + 1 
+                #Could be the ans
+                res = mid 
             else : 
                 r = mid - 1 
-        return r 
+        return res 
 
