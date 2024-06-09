a184091d366439229abd439a5ca1e50ea632a812
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-09 16:22:57 +0530

    Time: 58 ms (23.41%), Space: 16.5 MB (33.64%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,18 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-
-
-
         l , r = 1 , x 
 
         while l <= r : 
-            mid = l + (r - l) // 2 
+            
+            mid = (l + r) // 2
             target = x // mid 
 
+            print(l ,r , mid , target)
             if target == mid : 
                 return mid 
             elif target > mid : 
                 l = mid + 1 
             else : 
                 r = mid - 1 
-        #Return floor value if not found 
-        return r
+        return r 
+
