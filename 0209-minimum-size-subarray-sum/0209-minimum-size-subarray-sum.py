bc60940fb9b601e1d16ea76c8f7d940219b775ea
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-06-11 14:07:23 +0530

    Time: 183 ms (94.04%), Space: 30.3 MB (29.27%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        #Basically , I will take a sliding window  with l and r , r moves right until sum is less than target , the moment subarray becomes greater than or equal to target , l is incremented , this is because at that stage it is the minimum length subarray for which condition is valid 
-        l ,r = 0 , 0
-        res = float('inf')
+        l = 0 
+        res = float("inf")
         curSum = 0 
-        while r < len(nums) :
+        for r in range(len(nums)):
             curSum += nums[r]
-            while curSum >= target : 
-                res = min(res, (r-l+1))
-                curSum -= nums[l]
-                l += 1 
-            r+=1 
 
-        return 0 if res == float('inf') else res
+            while curSum >= target : 
+                res = min(res , r - l + 1)
+                curSum -= nums[l]
+                l += 1   
+
+        return res if res != float("inf") else 0
