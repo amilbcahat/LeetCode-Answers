0e205908894b339fa5301c8d856786679c3dfd3c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-07-14 17:08:31 +0530

    Time: 79 ms (15.16%), Space: 16.7 MB (21.31%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -14,7 +14,7 @@ class Solution:
             dfs(i + 1 , cur , total + candidates[i])
 
             cur.pop()
-            #Skip the duplicate branches 
+            #Skip the duplicate branches , same as Subsets 2
             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
                 i += 1 
             dfs(i + 1 , cur , total)
