37c4ccb79e15702cc0f7b4aa9dc6920556f569ac
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-07-14 17:19:41 +0530

    Time: 57 ms (69.06%), Space: 16.7 MB (21.31%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,22 +2,42 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         candidates.sort()
-        def dfs(i ,cur ,total):
-            if total == target : 
+        def dfs(pos , cur , target):
+            if target == 0 : 
                 res.append(cur.copy())
                 return 
 
-            if i >= len(candidates) or total > target : 
+            if target <= 0 : 
                 return 
 
-            cur.append(candidates[i])
-            dfs(i + 1 , cur , total + candidates[i])
+            prev = -1 
+            for i in range(pos , len(candidates)):
+                if prev == candidates[i]:
+                    continue
+                cur.append(candidates[i])
+                dfs(i + 1 , cur , target - candidates[i])
+                cur.pop()
+                prev = candidates[i]
 
-            cur.pop()
-            #Skip the duplicate branches , same as Subsets 2
-            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
-                i += 1 
-            dfs(i + 1 , cur , total)
+        dfs(0, [] ,target)
+        return res
         
-        dfs(0 , [] , 0)
-        return res 
+        # def dfs(i ,cur ,total):
+        #     if total == target : 
+        #         res.append(cur.copy())
+        #         return 
+
+        #     if i >= len(candidates) or total > target : 
+        #         return 
+
+        #     cur.append(candidates[i])
+        #     dfs(i + 1 , cur , total + candidates[i])
+
+        #     cur.pop()
+        #     #Skip the duplicate branches , same as Subsets 2
+        #     while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+        #         i += 1 
+        #     dfs(i + 1 , cur , total)
+        
+        # dfs(0 , [] , 0)
+        # return res 
