bf2283a487a8a79841f5b775918e92c38daa6b76
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-07-15 04:48:07 +0530

    Time: 23 ms (99.31%), Space: 16.3 MB (89.73%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(arr):
+            rob1 , rob2 = 0, 0 
+            #[rob1, rob2 , n , n+1 , n+2]
+        
+            for n in arr:
+                temp = max(n+rob1 ,rob2)
+                rob1 = rob2
+                rob2 = temp
+            return rob2  
+        #Just avoid take last and first element together ! 
+        #Added nums[0] here to take care of the edge case of array of size 1 
+        return max(nums[0],helper(nums[1:]),helper(nums[:-1]))
