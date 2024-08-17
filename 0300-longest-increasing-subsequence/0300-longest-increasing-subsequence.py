16bffe818ad49781b1b13a2c6a4330b50a389c3e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-17 22:57:50 +0530

    Time: 1886 ms (20.89%), Space: 16.8 MB (66.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,16 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+
         LIS = [1] * len(nums)
 
-        for i in range(len(nums) - 1 , -1 , -1):
+        for i in range(len(nums) - 1, -1 , -1):
             for j in range(i + 1 , len(nums)):
-                if nums[i] < nums[j]:
+                if nums[j] > nums[i] : 
+                    LIS[i] = max(LIS[i] , 1 + LIS[j])
                     #Check with prev element (who are bigger than current element)
                     #If found then we take max of current (to compare all conditions) and 1 + prev elem subproblem
                     #1 + is done to account for the added length and updated value !
-                    LIS[i] = max(LIS[i] , 1 + LIS[j])
+                    
 
-        print(LIS)
+        
         #LIS[0] is not taken because , it is possible tha nums[i] < nums[j] does not hold everytime , so we mess up the 
         #solution , and solution can be anywhere in the array !
         return max(LIS)
