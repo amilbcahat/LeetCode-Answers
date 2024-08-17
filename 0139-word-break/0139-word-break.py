9354b19dad71336d0fe11afd0b2d21f9581dfdd5
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-17 22:49:28 +0530

    Time: 36 ms (80.63%), Space: 16.7 MB (48.11%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        #considering that we got a valid one at last position !
+        dp[len(s)] = True 
+
+        for i in range(len(s) - 1 , -1 , -1) : 
+            for w in wordDict : 
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w : 
+                    dp[i] = dp[i + len(w)]
+                if dp[i] : 
+                    break 
+
+        #If all words are in WordDict then it will reach True first position as well 
+        return dp[0]        
