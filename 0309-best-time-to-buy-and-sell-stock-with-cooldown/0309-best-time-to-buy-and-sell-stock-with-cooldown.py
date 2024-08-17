ae9914c50699fc8a949e732bc1029a0b92e4b806
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-18 00:13:09 +0530

    Time: 44 ms (62.23%), Space: 17.9 MB (34.84%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,29 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #State : Buying or Selling ? 
-        # If Buy -> i+1
-        # If Sell -> i+2 (+1 for necessary cooldown)
-        dp = {} #key = (i , buying) val = max_profit 
+        dp = {}
 
-        def dfs(i , buying):
-            #Empty Array Condition 
-            if i>=len(prices):
+        def dfs(i , buying) : 
+            if i >= len(prices) : 
                 return 0 
-            
-            if (i , buying) in dp: 
+
+            if (i , buying) in dp : 
                 return dp[(i , buying)]
 
-            if buying:
-                #Next buy will be False 
-                buy = dfs(i+1, not buying) - prices[i]
-                cooldown = dfs(i+1 , buying)
-                dp[(i,buying)] = max(buy , cooldown)
+            if buying : 
+                buy = dfs(i + 1 , not buying) - prices[i]
+                cooldown = dfs(i + 1 , buying)
+                res = max(buy , cooldown)
             else : 
-                #Next buy will be True 
-                sell = dfs(i+2, not buying) + prices[i]
-                cooldown = dfs(i+1 , buying)
-                dp[(i,buying)] = max(sell , cooldown)
-            
-            return dp[(i, buying)]
+                sell = dfs(i + 2 , not buying) + prices[i]
+                cooldown = dfs(i + 1 , buying)
+                res = max(sell , cooldown)
 
+            dp[(i , buying)] = res
+            return res
+        
         return dfs(0 , True)
