7490db3e934349c652c120fe3da3c9fa281c41fc
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-17 19:53:27 +0530

    Time: 139 ms (91.59%), Space: 16.4 MB (94.92%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,19 +2,19 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0 
 
-        for i in range(len(s)):
-            #Odd Palindromes 
-            res += self.countPali(s , i , i)
-            #Even Palindromes 
+        for i in range(len(s)) : 
+            #Old Palindromes
+            res += self.countPali(s , i , i )
+            #Even Palidromes
             res += self.countPali(s , i , i + 1)
-        
+
         return res 
 
-    def countPali(self, s , l , r) : 
+    def countPali(self , s, l , r) : 
         res = 0 
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            res += 1 
+        while l >= 0 and r < len(s) and s[l] == s[r] : 
+            res += 1
             l -= 1 
-            r += 1
+            r += 1 
 
         return res        
