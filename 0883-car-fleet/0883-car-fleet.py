8284e96f0163008041b50abe0ff4353b5885681a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-18 04:50:30 +0530

    Time: 733 ms (35.49%), Space: 36.7 MB (77.6%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [[p , s] for p , s in zip(position , speed)]
+
+        stack = [] 
+         
+        for p , s in sorted(pairs)[::-1]:
+            stack.append((target - p) / s) 
+            if len(stack) >= 2 and stack[-1] <= stack[-2] : 
+                #cur carries has more speed that last so they collide and become a fleet 
+                #cur car starts moving with the car infront !
+                stack.pop()
+        return len(stack)
