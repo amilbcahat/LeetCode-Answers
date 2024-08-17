edc52ca890999f17a288287315016327a4946a70
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-18 04:51:57 +0530

    Time: 803 ms (10.07%), Space: 36.8 MB (74.49%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -6,6 +6,7 @@ class Solution:
          
         for p , s in sorted(pairs)[::-1]:
             stack.append((target - p) / s) 
+            #length must be more or equal to 2 , because we need a condition for collision
             if len(stack) >= 2 and stack[-1] <= stack[-2] : 
                 #cur carries has more speed that last so they collide and become a fleet 
                 #cur car starts moving with the car infront !
