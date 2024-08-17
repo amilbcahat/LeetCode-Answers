0c09edb157c7bb46386688af889d532cdd38cc36
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-18 01:43:40 +0530

    Time: 181 ms (59.89%), Space: 16.8 MB (68.95%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,29 +1,49 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0] * (len(coins)+1) for i in range(amount+1)]
-        dp[0] = [1] * (len(coins)+1)
+        #Memory Optimal Pure DP (it follows the grid diagram here) :- 
+        dp = [0] * (amount + 1)
+        dp[0] = 1 
 
-        #Code Grid -> 
-        # amount ->  5   4   3   2   1   0 
-        #            0   0   0   0   0   1  1             <- x amount can be made using 1,2,5 coins    
-        #            0   0   0   0   0   1  2   ^         <- x amount can be made using 2,5 coins
-        #            0   0   0   0   0   1  5   |         <- x amount can be made using 5 coin
-        #            0   0   0   0   0   1    coins 
-        # Last one is dummy row for skipping condition edge case and the matrix is initialized inverted this , representation is just to imitate knapsack pattern ! 
-        # Code implemented is inverting this 
-        
-        for a in range(1 , amount + 1):
-            for i in range(len(coins) - 1 , -1 , -1):
-                #Skipping the current coin (just look one below , and see if they reach to that amount)
-                #Seeing below (as per diagram)
-                dp[a][i] = dp[a][i + 1]
-                #Taking the current coin into consideration (just look to right on index equal to a-coins[i])
-                #Seeing on right side (as per diagram)
+        for i in range(len(coins) - 1, -1 , -1) : 
+            nextDp = [0] * (amount + 1)
+            nextDp[0] = 1 
+            #Skipping the current coin (just look one below , and see if they reach to that amount)
+        #.  #Seeing below (as per diagram)
+            for a in range(1 , amount + 1) : 
+                nextDp[a] = dp[a]
+                 #Taking the current coin into consideration (just look to right on index equal to a-coins[i])
+        #         #Seeing on right side (as per diagram)
                 if a - coins[i] >= 0 : 
-                    dp[a][i] += dp[a - coins[i]][i]
+                    nextDp[a] += nextDp[a - coins[i]]
+            dp = nextDp 
 
-        return dp[amount][0]
+        return dp[amount]
 
+        # dp = [[0] * (len(coins)+1) for i in range(amount+1)]
+        # dp[0] = [1] * (len(coins)+1)
+
+        # #Code Grid -> 
+        # # amount ->  5   4   3   2   1   0 
+        # #            0   0   0   0   0   1  1             <- x amount can be made using 1,2,5 coins    
+        # #            0   0   0   0   0   1  2   ^         <- x amount can be made using 2,5 coins
+        # #            0   0   0   0   0   1  5   |         <- x amount can be made using 5 coin
+        # #            0   0   0   0   0   1    coins 
+        # # Last one is dummy row for skipping condition edge case and the matrix is initialized inverted this , representation is just to imitate knapsack pattern ! 
+        # # Code implemented is inverting this 
+        
+        # for a in range(1 , amount + 1):
+        #     for i in range(len(coins) - 1 , -1 , -1):
+        #         #Skipping the current coin (just look one below , and see if they reach to that amount)
+        #         #Seeing below (as per diagram)
+        #         dp[a][i] = dp[a][i + 1]
+        #         #Taking the current coin into consideration (just look to right on index equal to a-coins[i])
+        #         #Seeing on right side (as per diagram)
+        #         if a - coins[i] >= 0 : 
+        #             dp[a][i] += dp[a - coins[i]][i]
+
+        # return dp[amount][0]
+
+        #Easiest and most elegant
         # dp = [0] * (amount + 1)
         # dp[0] = 1 #number of way to reach 0 denomination 
 
