6028f9a7bd8e8adedeceb91380c9c6935877db65
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-08-18 01:32:16 +0530

    Time: 258 ms (44.37%), Space: 26.8 MB (53.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # dp = [[0] * (len(coins)+1) for i in range(amount+1)]
-        # dp[0] = [1] * (len(coins)+1)
+        dp = [[0] * (len(coins)+1) for i in range(amount+1)]
+        dp[0] = [1] * (len(coins)+1)
 
         #Code Grid -> 
         # amount ->  5   4   3   2   1   0 
@@ -10,29 +10,29 @@ class Solution:
         #            0   0   0   0   0   1  5   |         <- x amount can be made using 5 coin
         #            0   0   0   0   0   1    coins 
         # Last one is dummy row for skipping condition edge case and the matrix is initialized inverted this , representation is just to imitate knapsack pattern ! 
+        # Code implemented is inverting this 
         
-        # for a in range(1, amount+1):
-        #     for i in range(len(coins)-1,-1,-1):
-        #         #Skipping the current coin (just look one below)
-        #         dp[a][i] = dp[a][i+1]
-        #         #Taking the current coin into consideration (just look to right on index equal to a-coins[i])
-        #         if (a-coins[i]) >= 0 : 
-        #             dp[a][i] += dp[a-coins[i]][i]
-        # return dp[amount][0]
+        for a in range(1 , amount + 1):
+            for i in range(len(coins) - 1 , -1 , -1):
+                #Skipping the current coin (just look one below , and see if they reach to that amount)
+                #Seeing below (as per diagram)
+                dp[a][i] = dp[a][i + 1]
+                #Taking the current coin into consideration (just look to right on index equal to a-coins[i])
+                #Seeing on right side (as per diagram)
+                if a - coins[i] >= 0 : 
+                    dp[a][i] += dp[a - coins[i]][i]
 
-        # Initialize dp array where dp[a] represents the number of ways to make amount 'a'
-            # Initialize dp array where dp[a] represents the number of ways to make amount 'a'
-        dp = [0] * (amount + 1)
-        dp[0] = 1  # Base case: One way to make amount 0 (by using no coins)
-        
-        # Iterate through each coin
-        for coin in coins:
-            # Update the dp array for all amounts from coin to amount
-            for a in range(coin, amount + 1):
-                dp[a] += dp[a - coin]
-        
-        # The result is the number of ways to make the target amount
-        return dp[amount]
+        return dp[amount][0]
+
+        # dp = [0] * (amount + 1)
+        # dp[0] = 1 #number of way to reach 0 denomination 
+
+        # #Maintains order for not counting permutation of sum of coins and thats why outer loop has to be coins 
+        # for c in coins : 
+        #     for a in range(c , amount + 1): 
+        #         dp[a] += dp[a - c]
+
+        # return dp[amount]
 
 
         # dp = {}
