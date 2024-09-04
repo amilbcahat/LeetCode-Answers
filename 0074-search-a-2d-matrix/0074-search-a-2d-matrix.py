ff7c713ad58178604e70ffc363ae651668491dc6
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2024-09-05 00:31:58 +0530

    Time: 48 ms (39.83%), Space: 17.2 MB (21.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,28 +1,50 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #Striver Solution -> 
+        #Flatten to 1D Array then solve 
         rows , cols = len(matrix) , len(matrix[0])
-        top , bot = 0 , rows - 1
-        while top <= bot : 
-            row = (top + bot) // 2 
-            if target > matrix[row][-1] : 
-                top = row + 1 
-            elif target < matrix[row][0] : 
-                bot = row - 1
-            else : 
-                break 
+        l , r = 0 , (rows * cols)- 1
 
-        if not top <= bot : 
-            return False 
-            #Did not Found 
-
-        l , r = 0 , cols - 1
         while l <= r : 
             mid = (l + r) // 2 
-            if target < matrix[row][mid] : 
+
+            #We flatten to 1D array by this logic always 
+            row = mid // cols 
+            col = mid % cols 
+
+            if matrix[row][col] > target : 
                 r = mid - 1
-            elif target > matrix[row][mid] : 
+            elif matrix[row][col] < target : 
                 l = mid + 1
             else : 
-                return True 
-
+                return True
         return False 
+
+        #NeetCode solution -> 
+        # rows , cols = len(matrix) , len(matrix[0])
+
+        # top = 0
+        # bot = rows - 1
+
+        # while top <= bot : 
+        #     row = (top + bot) // 2 
+        #     if target > matrix[row][-1] : 
+        #         top = row + 1
+        #     elif target < matrix[row][0] : 
+        #         bot = row - 1 
+        #     else : 
+        #         break 
+
+        # if top > bot : 
+        #     return False 
+
+        # l , r = 0 , cols - 1
+        # while l <= r : 
+        #     mid = (l + r) // 2 
+        #     if target > matrix[row][mid] : 
+        #         l = mid + 1 
+        #     elif target < matrix[row][mid] : 
+        #         r = mid - 1
+        #     else : 
+        #         return True 
+        # return False 
