92d8ec1fc93186b372637591de5d2d587739db34
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-05 02:41:38 +0530

    Time: 213 ms (5.3%), Space: 29.5 MB (27.65%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -7,40 +7,40 @@ class Solution:
             else: 
                 return 1
 
-        #Shifted Negs to Last
-        l , r = 0 , len(nums) - 1
-        while l < r : 
-            while l < r and l < len(nums) - 1 and nums[l] > 0 : 
-                l += 1 
+        # #Shifted Negs to Last
+        # l , r = 0 , len(nums) - 1
+        # while l < r : 
+        #     while l < r and l < len(nums) - 1 and nums[l] > 0 : 
+        #         l += 1 
             
-            while l < r and r > 1 and  nums[r] <= 0: 
-                r -= 1
+        #     while l < r and r > 1 and  nums[r] <= 0: 
+        #         r -= 1
             
-            if l < r and nums[l] <= 0 and nums[r] > 0:
-                nums[l], nums[r] = nums[r], nums[l]
+        #     if l < r and nums[l] <= 0 and nums[r] > 0:
+        #         nums[l], nums[r] = nums[r], nums[l]
             
-            l += 1 
-            r -= 1
+        #     l += 1 
+        #     r -= 1
 
-        ind = -1 
-        for i in range(len(nums)): 
-            if nums[i] <= 0: 
-                ind = i
-                break
+        # ind = -1 
+        # for i in range(len(nums)): 
+        #     if nums[i] <= 0: 
+        #         ind = i
+        #         break
 
-        if ind == -1:
-            #If all positive
-            r = len(nums) - 1
-        else : 
-            #pos - neg boundary
-            r = ind - 1
+        # if ind == -1:
+        #     #If all positive
+        #     r = len(nums) - 1
+        # else : 
+        #     #pos - neg boundary
+        #     r = ind - 1
 
         #Cylic sort to sort pos numbers, index based
-        print(r)
         i = 0 
-        while i <= r: 
+        while i < len(nums): 
+            print(i)
             correctIndx = nums[i] - 1
-            if nums[i] < r + 1  and nums[i] != nums[correctIndx]: 
+            if nums[i] > 0 and nums[i] <= len(nums)  and nums[i] != nums[correctIndx]: 
                 nums[i], nums[correctIndx] = nums[correctIndx], nums[i]
             else: 
                 i += 1
@@ -48,7 +48,7 @@ class Solution:
         print(nums)
         
         #Then finding the missing number 
-        for i in range(r + 1): 
+        for i in range(len(nums)): 
             if nums[i] != i + 1:
                 return i + 1
-        return r + 2 #+1 , to manage index and +1 to tell for the case where missing number is more than the length of pos number subarray
+        return len(nums) + 1#+1 , to manage index and +1 to tell for the case where missing number is more than the length of pos number subarray
