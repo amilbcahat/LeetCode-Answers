989ee857d71f284b69314b5ea24adcaf8ff4f01d
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-05 02:42:38 +0530

    Time: 188 ms (5.3%), Space: 29.3 MB (35.62%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,40 +1,5 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        #Base Case Handling
-        # if len(nums) == 1 : 
-        #     if nums[0] == 1 : 
-        #         return 2 
-        #     else: 
-        #         return 1
-
-        # #Shifted Negs to Last
-        # l , r = 0 , len(nums) - 1
-        # while l < r : 
-        #     while l < r and l < len(nums) - 1 and nums[l] > 0 : 
-        #         l += 1 
-            
-        #     while l < r and r > 1 and  nums[r] <= 0: 
-        #         r -= 1
-            
-        #     if l < r and nums[l] <= 0 and nums[r] > 0:
-        #         nums[l], nums[r] = nums[r], nums[l]
-            
-        #     l += 1 
-        #     r -= 1
-
-        # ind = -1 
-        # for i in range(len(nums)): 
-        #     if nums[i] <= 0: 
-        #         ind = i
-        #         break
-
-        # if ind == -1:
-        #     #If all positive
-        #     r = len(nums) - 1
-        # else : 
-        #     #pos - neg boundary
-        #     r = ind - 1
-
         #Cylic sort to sort pos numbers, index based
         i = 0 
         while i < len(nums): 
@@ -44,11 +9,9 @@ class Solution:
                 nums[i], nums[correctIndx] = nums[correctIndx], nums[i]
             else: 
                 i += 1
-
-        print(nums)
         
         #Then finding the missing number 
         for i in range(len(nums)): 
             if nums[i] != i + 1:
                 return i + 1
-        return len(nums) + 1#+1 , to manage index and +1 to tell for the case where missing number is more than the length of pos number subarray
+        return len(nums) + 1
