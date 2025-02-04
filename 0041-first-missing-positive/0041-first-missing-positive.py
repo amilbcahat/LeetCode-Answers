79b04177fe65cba4213f8d0f5f272d709f92bd7b
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-05 02:11:18 +0530

    Time: 93 ms (5.3%), Space: 29.5 MB (31.36%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,50 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        if len(nums) == 1 : 
+            if nums[0] == 1 : 
+                return 2 
+            else: 
+                return 1
+
+        #Shifted Negs to Last
+        l , r = 0 , len(nums) - 1
+        while l < r : 
+            while l < r and l < len(nums) - 1 and nums[l] > 0 : 
+                l += 1 
+            
+            while l < r and r > 1 and  nums[r] <= 0: 
+                r -= 1
+            
+            if l < r and nums[l] <= 0 and nums[r] > 0:
+                nums[l], nums[r] = nums[r], nums[l]
+            
+            l += 1 
+            r -= 1
+
+        ind = -1 
+        for i in range(len(nums)): 
+            if nums[i] <= 0: 
+                ind = i
+                break
+        if ind == -1:
+            r = len(nums) - 1
+        else : 
+            r = ind - 1
+
+        #Cylic sort to sort pos numbers, index based
+        print(r)
+        i = 0 
+        while i <= r: 
+            correctIndx = nums[i] - 1
+            if nums[i] < r + 1  and nums[i] != nums[correctIndx]: 
+                nums[i], nums[correctIndx] = nums[correctIndx], nums[i]
+            else: 
+                i += 1
+
+        print(nums)
+        
+        #Then finding the missing number 
+        for i in range(r + 1): 
+            if nums[i] != i + 1:
+                return i + 1
+        return r + 2 #+1 , to manage index and +1 to tell for 
