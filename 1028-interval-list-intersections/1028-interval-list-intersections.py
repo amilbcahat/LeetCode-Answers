4567380452c40fe38d9d23c8586828a460eb56c9
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-11 03:31:43 +0530

    Time: 13 ms (5.85%), Space: 18.8 MB (21.64%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -13,14 +13,11 @@ class Solution:
             return p1, p2
 
         while p1 < len(firstList) and p2 < len(secondList):
-            if firstList[p1][1] < secondList[p2][0]:
-                p1, p2 = greedyChoice(p1, p2)
-
-            elif firstList[p1][0] > secondList[p2][1]: 
-                p1, p2 = greedyChoice(p1, p2)
-            else:
+            if max(firstList[p1][0], secondList[p2][0]) <= min(firstList[p1][1], secondList[p2][1]):
                 #overlapped !!
                 res.append([max(firstList[p1][0], secondList[p2][0]), min(firstList[p1][1], secondList[p2][1])])
+                p1, p2 = greedyChoice(p1, p2)
+            else: 
                 p1, p2 = greedyChoice(p1, p2)
 
         return res
