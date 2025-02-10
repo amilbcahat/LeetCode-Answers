e01601e8a710747685ba32b7e2ee849313a6f4e5
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-11 03:29:16 +0530

    Time: 7 ms (52.56%), Space: 18.6 MB (38.14%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -2,32 +2,25 @@ class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         p1 , p2 = 0 , 0 
         res = []
+        def greedyChoice(p1, p2):
+            if firstList[p1][1] < secondList[p2][1]:
+                p1 += 1
+            elif firstList[p1][1] > secondList[p2][1]:
+                p2 += 1
+            else: 
+                p1 += 1
+                p2 += 1
+            return p1, p2
+
         while p1 < len(firstList) and p2 < len(secondList):
             if firstList[p1][1] < secondList[p2][0]:
-                if firstList[p1][1] < secondList[p2][1]:
-                    p1 += 1
-                elif firstList[p1][1] > secondList[p2][1]:
-                    p2 += 1
-                else: 
-                    p1 += 1
-                    p2 += 1
+                p1, p2 = greedyChoice(p1, p2)
+
             elif firstList[p1][0] > secondList[p2][1]: 
-                if firstList[p1][1] < secondList[p2][1]:
-                    p1 += 1
-                elif firstList[p1][1] > secondList[p2][1]:
-                    p2 += 1
-                else: 
-                    p1 += 1
-                    p2 += 1
+                p1, p2 = greedyChoice(p1, p2)
             else:
                 #overlapped !!
                 res.append([max(firstList[p1][0], secondList[p2][0]), min(firstList[p1][1], secondList[p2][1])])
-                if firstList[p1][1] < secondList[p2][1]:
-                    p1 += 1
-                elif firstList[p1][1] > secondList[p2][1]:
-                    p2 += 1
-                else: 
-                    p1 += 1
-                    p2 += 1
+                p1, p2 = greedyChoice(p1, p2)
 
         return res
