9943a562b8bdb34130ce60cc91ee4ce0fa8e5317
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-16 14:10:49 +0530

    Time: 1216 ms (5.26%), Space: 95.5 MB (15.79%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
@@ -1,27 +1,23 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         adj = defaultdict(list)
-        indegree = defaultdict(int)
         nodes = set()
         for i in range(len(edges)):
             src = i
             dst = edges[src] 
             if dst != -1:
                 adj[src].append(dst)
-                indegree[dst] += 1
                 nodes.add(src)
                 nodes.add(dst)
 
         print(adj)
         visited = set()
         path = set()
-        # print(nodes)
         res = -1
         def dfs(node, nodeIndex, lenOfPath):
             nonlocal res
             if node in path:
                 res = max(res ,lenOfPath - nodeIndex[node])
-                # print(path, nodeIndex)
                 return lenOfPath 
 
             if node in visited: 
