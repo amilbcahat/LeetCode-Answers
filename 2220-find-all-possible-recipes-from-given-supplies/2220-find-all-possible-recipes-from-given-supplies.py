b1d29d6b32293c261162a27d7b72c0a2ff33faeb
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-17 20:07:28 +0530

    Time: 94 ms (42.72%), Space: 19.7 MB (62.34%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        adj = defaultdict(list)
+        indegree = defaultdict(int)
+        for i in range(len(recipes)): 
+            for ind in ingredients[i]:
+                src = recipes[i]
+                pre = ind 
+                adj[ind].append(src)
+                indegree[src] += 1
+
+        lookup = set(recipes)
+        res = set()
+        
+        queue = deque()
+        for s in supplies: 
+            queue.append(s)
+
+        while queue: 
+            node = queue.popleft()
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0: 
+                    if nei in lookup: 
+                        res.add(nei)
+                    queue.append(nei)
+
+        return list(res)
