9813eeefe620e042ce48b89f30389b9e378bc8d0
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-19 02:23:43 +0530

    Time: 38 ms (76.34%), Space: 38.1 MB (37.5%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,46 @@
+class UnionFind: 
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+        self.comp = n
+
+        for i in range(0, n):
+            self.par[i] = i
+            self.rank[i] = 0
+
+    def find(self, n):
+        p = self.par[n]
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p 
+
+    def union(self, n1, n2): 
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2 : 
+            return False
+
+        if self.rank[p1] > self.rank[p2]: 
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]: 
+            self.par[p1] = p2 
+        else: 
+            self.par[p1] = p2 
+            self.rank[p2] += 1
+
+        #Decrement individual components on union 
+        self.comp -= 1
+        return True
+        
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1: 
+            #Edges are anyway less
+            return -1 
+
+        uf = UnionFind(n)
+
+        for src, dst in connections: 
+            uf.union(src, dst)
+
+        return uf.comp - 1
