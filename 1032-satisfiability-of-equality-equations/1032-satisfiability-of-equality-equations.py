8ac44be85bd8a0e98a6bbb6a786f2089662af209
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-19 23:54:04 +0530

    Time: 5 ms (13.42%), Space: 17.9 MB (81.86%) - LeetHub

---
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.py
@@ -0,0 +1,49 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        par = {}
+        rank = {}
+        
+        for eq in equations: 
+            par[eq[0]] = eq[0]
+            par[eq[3]] = eq[3]
+            rank[eq[0]] = 0
+            rank[eq[3]] = 0 
+
+        def find(n):
+            p = par[n]
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p
+
+        def union(n1 , n2):
+            p1 , p2 = find(n1) , find(n2)
+            if p1 == p2:
+                return False 
+
+            if rank[p1] < rank[p2]: 
+                par[p1] = p2 
+            elif rank[p2] < rank[p1]: 
+                par[p2] = p1
+            else:
+                par[p1] = p2
+                rank[p2] += 1
+            return True 
+
+        for eq in equations:
+            if eq[1] == "=" and eq[2] == "=": 
+                ch1 = eq[0]
+                ch2 = eq[3]
+                union(ch1, ch2)
+
+        for eq in equations:
+            if eq[1] != "=" or eq[2] != "=": 
+                ch1 = eq[0]
+                ch2 = eq[3]
+                p1 = find(ch1)
+                p2 = find(ch2)
+                print(p1, p2)
+                if p1 == p2: 
+                    return False
+        print(par)
+        return True 
