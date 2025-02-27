4e3dfb516a5bd0df41561f0baf744eb52dd4daa5
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-02-28 03:15:56 +0530

    Time: 4 ms (19.87%), Space: 17.9 MB (47.71%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        #This is a preemption algo 
         numCounter = Counter(s)
         maxHeap = []
         for key in numCounter:
@@ -9,15 +10,19 @@ class Solution:
         lastAdj = ""
         lastAdjCount = 0
         while maxHeap: 
+            #current letter
             cnt1 , ch1 = heapq.heappop(maxHeap)
+            #when similar adj what will happen is nothing will be left in maxHeap, so it will move out of this loop 
             if lastAdj and lastAdjCount > 0:
+                #last letter now can be adjacent again
                 heapq.heappush(maxHeap, (-lastAdjCount, lastAdj)) 
             st += ch1
             cnt1 = -1 * cnt1
             cnt1 -= 1
 
+            #set this as last adj 
             lastAdj = ch1
             lastAdjCount = cnt1 
-        
+        print(st)
         return st if len(st) == len(s) else ""
             
