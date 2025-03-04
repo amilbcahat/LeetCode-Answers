284f5fe0bdcd10faa2239dfacb35c6d566bbb00c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-05 03:25:02 +0530

    Time: 1282 ms (58.43%), Space: 65.8 MB (34.45%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -16,26 +16,24 @@ class WordDictionary:
         cur.endOfWord = True    # Mark the last node as the end of a word
         
     def search(self, word: str) -> bool:
-        def dfs(i, node):
-            # Base case: if we've reached the end of the word
-            if i == len(word):
-                return node.endOfWord  # Return True only if this node is marked as end of a word
-                
-            if word[i] == ".":  # Handle wildcard character
-                # Try all possible paths by checking all children
-                for n in node.children:
-                    if dfs(i + 1, node.children[n]):  # Recursively check the next character
-                        return True  # Return True if any path leads to a match
-                return False  # No match found after checking all children
-            else:  # Handle regular character
-                if word[i] in node.children:  # Check if the character exists in current node's children
-                    char = word[i]
-                    return dfs(i + 1, node.children[char])  # Continue with the next character
-                else:
-                    return False  # Character not found, no match possible
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)): 
+                c = word[i]
+                if c == ".": 
+                    for child in cur.children.values(): 
+                        if dfs(i + 1, child): 
+                            return True 
+                    return False
+                else: 
+                    if c not in cur.children: 
+                        return False 
+                    cur = cur.children[c]
+            
+            return cur.endOfWord
+        
+        return dfs(0, self.root)
                     
-        return dfs(0, self.root)  # Start DFS from index 0 and root node
-
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
