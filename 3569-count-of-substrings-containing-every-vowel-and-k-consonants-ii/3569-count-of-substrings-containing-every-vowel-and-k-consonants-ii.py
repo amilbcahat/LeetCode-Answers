18b9f96e1ff335418461f6e8aab634a1827582ae
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-11 07:51:52 +0530

    Time: 1603 ms (73.99%), Space: 18.5 MB (91.33%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -5,8 +5,6 @@ class Solution:
 
         def atLeastK(k):
             vowelMap = {}
-            conMap = {}
-
             l = 0 
             res = 0 
             con = 0
@@ -15,19 +13,15 @@ class Solution:
                 if s[r] in vowels:
                     vowelMap[s[r]] = 1 + vowelMap.get(s[r], 0)
                 else: 
-                    conMap[s[r]] = 1 + conMap.get(s[r], 0)
                     con += 1
 
                 while len(vowelMap) == 5 and con >= k:
-                    res += (len(s) - r)
+                    res += (len(s) - r) 
                     if s[l] in vowelMap:
                         vowelMap[s[l]] -= 1
                         if vowelMap[s[l]] == 0:
                             del vowelMap[s[l]]
                     else:
-                        conMap[s[l]] -= 1
-                        if conMap[s[l]] == 0:
-                            del conMap[s[l]]
                         con -= 1
                     l += 1
 
