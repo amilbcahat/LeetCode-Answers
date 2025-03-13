57024bd2beab0ac472fa7a15370405b5a82e992e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-13 05:48:36 +0530

    Time: 1 ms (29.29%), Space: 17.8 MB (43.28%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -8,27 +8,56 @@ class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root: 
             return []
-
-        q = deque([(root, 0)])
+        # DFS traversal 
         verticalMap = defaultdict(list)
         max_col = float("-inf")
         min_col = float("inf")
-        while q: 
-            for i in range(len(q)):
-                #We track and map with the x coordinate
-                node, x = q.popleft()
-                verticalMap[x].append(node.val)
-                max_col = max(x, max_col)
-                min_col = min(x, min_col)
-                if node and node.left:
-                    q.append([node.left, x - 1])
-                if node and node.right:
-                    q.append([node.right, x + 1])
+        def dfs(node, r, c):
+            nonlocal max_col, min_col
+            if not node: 
+                return 
 
-        length = max_col - min_col + 1
+            max_col = max(max_col, c)
+            min_col = min(min_col, c)
+            verticalMap[c].append((r, node.val))
+            dfs(node.left, r + 1, c - 1)
+            dfs(node.right, r + 1, c + 1)
 
-        res = [[]] * length
-        for key in verticalMap.keys():
-            res[key - min_col] = verticalMap[key]
+        dfs(root, 0, 0)
 
-        return res 
+        print(verticalMap, min_col, max_col)
+        res = []
+        for col in range(min_col, max_col + 1): 
+            verticalMap[col].sort(key=lambda x: x[0])
+            colVals = [val for row, val in verticalMap[col]]
+            res.append(colVals)
+
+        return res
+
+        # BFS traversal
+        # if not root: 
+        #     return []
+
+        # q = deque([(root, 0)])
+        # verticalMap = defaultdict(list)
+        # max_col = float("-inf")
+        # min_col = float("inf")
+        # while q: 
+        #     for i in range(len(q)):
+        #         #We track and map with the x coordinate
+        #         node, x = q.popleft()
+        #         verticalMap[x].append(node.val)
+        #         max_col = max(x, max_col)
+        #         min_col = min(x, min_col)
+        #         if node and node.left:
+        #             q.append([node.left, x - 1])
+        #         if node and node.right:
+        #             q.append([node.right, x + 1])
+
+        # length = max_col - min_col + 1
+
+        # res = [[]] * length
+        # for key in verticalMap.keys():
+        #     res[key - min_col] = verticalMap[key]
+
+        # return res 
