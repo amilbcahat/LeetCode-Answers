61f7e6aead9c4fe2c0b3dab5872c5708354804a8
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-17 22:02:33 +0530

    Time: 0 ms (100%), Space: 17.8 MB (54.83%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -1,13 +1,29 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        # time = 0
+        # some = tickets 
+        # while tickets[k] != 0: 
+        #     for t in range(len(tickets)): 
+        #         if tickets[t] > 0: 
+        #             time += 1
+        #             tickets[t] -= 1
+        #             print(time, tickets)
+        #             if t == k and tickets[t] == 0: 
+        #                 print("boom")
+        #                 # return time 
+
+        required = tickets[k]
         time = 0
-        while tickets[k] != 0: 
-            for t in range(len(tickets)): 
-                if tickets[t] > 0: 
-                    time += 1
-                    tickets[t] -= 1
-                    if t == k and tickets[t] == 0: 
-                        return time 
+        for i in range(len(tickets)): 
+            if i <= k: 
+                time += required if tickets[i] > required else tickets[i]
+            else: 
+                time += required - 1 if tickets[i] >= required else tickets[i]
+
+        return time 
+        
+
+
 
         
 
