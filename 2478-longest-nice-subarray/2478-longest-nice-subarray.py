7e279c3e6efbeb21a0fe5859cb0449a633d952f5
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-18 20:21:58 +0530

    Time: 91 ms (61.28%), Space: 33.5 MB (33.84%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        #Logic is simple , if a number has some set bit , then at that place you cant have a set bit 
+        used_bits = 0 
+        res = 0
+        l = 0
+        for r in range(len(nums)): 
+            while used_bits & nums[r] != 0: 
+                used_bits ^= nums[l]
+                l += 1
+
+            used_bits |= nums[r]
+            res = max((r - l + 1), res)
+        return res
+
+            
