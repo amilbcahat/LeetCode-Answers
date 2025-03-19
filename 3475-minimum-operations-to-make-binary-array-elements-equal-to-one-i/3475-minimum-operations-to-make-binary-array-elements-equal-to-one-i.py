1cadd00c5bdbc8a767cceb7bf455656feff030e6
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-19 22:34:28 +0530

    Time: 183 ms (21.34%), Space: 22.2 MB (7.97%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         l = 0
-        flips = 0 
+        flips = 0 #Just look if first bit is not 1 then you need to change next bits !!
         for r in range(2, len(nums)): 
             if nums[l] == 0: 
                 for i in range(l, l + 3): 
