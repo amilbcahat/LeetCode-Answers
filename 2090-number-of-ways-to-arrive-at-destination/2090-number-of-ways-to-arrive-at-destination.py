19fe163563a8e7dbb5d3bb44e4f47e6946fce990
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-23 08:25:34 +0530

    Time: 75 ms (5.62%), Space: 30.3 MB (5.31%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
@@ -1,31 +1,53 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for s, d, w in roads: 
-            adj[s].append((d, w))
-            adj[d].append((s, w))
+        adj_list = [[] for _ in range(n)]
+        for u, v, t in roads:
+            adj_list[u].append((v, t))
+            adj_list[v].append((u, t))
 
-        minHeap = [(0, 0)]
-
-        MOD = 10 ** 9 + 7 
-
-        visit = set()
-        min_cost = [float("inf")] * n 
-        path_count = [0] * n 
-        path_count[0] = 1
-
-        while minHeap: 
-            cur_cost, cur_node = heapq.heappop(minHeap)
-                
-            for nei, nei_cost in adj[cur_node]: 
-                #Here we are not traversing , each and every node
-                #When we only push those nodes to heap, which we know , are min_cost 
-                if nei_cost + cur_cost < min_cost[nei]: 
-                    min_cost[nei] = nei_cost + cur_cost 
-                    path_count[nei] = path_count[cur_node]
-                    heapq.heappush(minHeap, (min_cost[nei], nei))
-                elif nei_cost + cur_cost == min_cost[nei]: 
-                #Otherwise we simply calculate, instead of traversing and increasing the TE for them 
-                    path_count[nei] = (path_count[nei] + path_count[cur_node]) % MOD
+        # Let P(n, t) = Number of ways to arrive at intersection n in exactly time t
+        # Let T(n) = The shortest time to reach intersction n
+        # P(n, T(n)) = Sum for e over edges of n (
+        #   P(edge(e), T(n) - time(e)) 
+        # )
+        # Base case:
+        # P(0, 0) = 1
+        # P(v, t) = 0 if T(v) != t
         
-        return path_count[n - 1]
+        START, DEST = 0, n - 1
+        shortest_t = [float('inf')] * n
+        visited = [False] * n
+        pq = [(0, START)]
+
+        while pq:
+            t, node = heapq.heappop(pq)
+            if visited[node]:
+                continue
+            visited[node] = True
+            shortest_t[node] = t
+
+            for nbr, dt in adj_list[node]:
+                heapq.heappush(pq, (t + dt, nbr))
+        
+        MOD = (10 ** 9) + 7
+        dp = {}
+        def num_ways(node: int, t: int) -> int:
+            if node == 0 and t == 0:
+                return 1
+            
+            if shortest_t[node] != t:
+                return 0
+
+            key = node
+            if key in dp:
+                return dp[key]
+
+            ways = 0
+            for nbr, dt in adj_list[node]:
+                ways += num_ways(nbr, t - dt)
+                ways %= MOD
+            
+            dp[key] = ways
+            return ways
+
+        return num_ways(DEST, shortest_t[DEST])
