70c17d542f3c1ac2a2fa4fea13177e7753afb02e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-26 21:08:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (17.11%) - LeetHub

---
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        prefixSum = 0
+        postfixSum = 0
+        arr = []
+        for i in range(len(nums)):
+            num = nums[i]
+            prefixSum += num 
+            arr.append(prefixSum)
+
+
+        for i in range(len(nums) - 1 , -1 , -1):
+            num = nums[i]
+            postfixSum += num 
+            arr[i] -= postfixSum
+            
+        print(arr)
+        for i in range(len(arr)):
+            if arr[i] == 0: 
+                return i
+
+        return -1 
