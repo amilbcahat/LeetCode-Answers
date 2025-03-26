25e194c94a284db2b724836478632741232aa161
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-27 01:17:24 +0530

    Time: 142 ms (61.19%), Space: 63.3 MB (19.05%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
@@ -1,11 +1,14 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         if len(nums) == 1: 
+            #If length is 1, and curSum is equal to k, then we got one subarray, otherwise 0 
             return 1 if k == nums[-1] else 0
 
         total = 0 
-        preMap = {0 : -1}
-        preArr = []
+        #Intuition
+        #This map contains curSum to i info
+        #Now the trick is , finding for prefix[i] - prefix[j] = k , this means there is a subarray that has sum equal to k , and that is between i and j 
+        preMap = {0 : -1} #We got curSum of 0, even before Array had started, so count the length from beginning
         res = 0
         for r, num in enumerate(nums): 
             total += num 
@@ -14,5 +17,5 @@ class Solution:
                 l = preMap[diff]
                 res = max(res, r - l)
             if total not in preMap: 
-                preMap[total] = r #For Maximum result 
+                preMap[total] = r #For Maximum result , have it set on the first encounter
         return res
