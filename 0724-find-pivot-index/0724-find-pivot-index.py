902691a3aaeb73585308b6bea48d0159246ce396
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-03-26 21:13:02 +0530

    Time: 5 ms (65.87%), Space: 18.5 MB (77.98%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,22 +1,32 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        prefixSum = 0
-        postfixSum = 0
-        arr = []
-        for i in range(len(nums)):
-            num = nums[i]
-            prefixSum += num 
-            arr.append(prefixSum)
+        S = sum(nums)
+        leftSum = 0
+        for i, x in enumerate(nums): 
+            rightSum = S - leftSum - x
+            if leftSum == rightSum: 
+                return i 
+            leftSum += x 
+
+        return -1
+        
+        # prefixSum = 0
+        # postfixSum = 0
+        # arr = []
+        # for i in range(len(nums)):
+        #     num = nums[i]
+        #     prefixSum += num 
+        #     arr.append(prefixSum)
 
 
-        for i in range(len(nums) - 1 , -1 , -1):
-            num = nums[i]
-            postfixSum += num 
-            arr[i] -= postfixSum
+        # for i in range(len(nums) - 1 , -1 , -1):
+        #     num = nums[i]
+        #     postfixSum += num 
+        #     arr[i] -= postfixSum
             
-        print(arr)
-        for i in range(len(arr)):
-            if arr[i] == 0: 
-                return i
+        # print(arr)
+        # for i in range(len(arr)):
+        #     if arr[i] == 0: 
+        #         return i
 
-        return -1 
+        # return -1 
