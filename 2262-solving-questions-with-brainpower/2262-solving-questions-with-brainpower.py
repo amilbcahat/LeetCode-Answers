5056b159d398844a625e457203a3a488aa35379e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-01 19:15:35 +0530

    Time: 179 ms (20.71%), Space: 65.3 MB (40.35%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        dp = {}
+        def dfs(i): 
+            if i >= len(questions): 
+                return 0 
+            
+            if i in dp: 
+                return dp[i]
+
+            points = 0
+            skip = dfs(i + 1)
+            include = questions[i][0] + dfs(i + (questions[i][1] + 1))
+            points += max(skip, include)
+
+            dp[i] = points
+            return points
+
+        return dfs(0)
