cbe3c1af1b615be423a113ff72859020ab3a9be9
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-01 20:26:16 +0530

    Time: 75 ms (77.45%), Space: 61 MB (50.81%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,18 +1,32 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        dp = {}
-        def dfs(i): 
-            if i >= len(questions): 
-                return 0 
+        #Iterative 
+        n = len(questions)
+        cache = [0] * n 
+
+        for i in reversed(range(n)): 
+            points, brainp = questions[i]
+            next_i = i + brainp + 1
+            include = points + (cache[next_i] if next_i < n else 0)
+            skip = cache[i + 1] if i + 1 < n else 0 
+            cache[i] = max(skip, include)
+        
+        return cache[0]
+
+        # Recursive 
+        # dp = {}
+        # def dfs(i): 
+        #     if i >= len(questions): 
+        #         return 0 
             
-            if i in dp: 
-                return dp[i]
+        #     if i in dp: 
+        #         return dp[i]
 
-            skip = dfs(i + 1)
-            include = questions[i][0] + dfs(i + (questions[i][1] + 1))
-            points = max(skip, include)
+        #     skip = dfs(i + 1)
+        #     include = questions[i][0] + dfs(i + (questions[i][1] + 1))
+        #     points = max(skip, include)
 
-            dp[i] = points
-            return points
+        #     dp[i] = points
+        #     return points
 
-        return dfs(0)
+        # return dfs(0)
