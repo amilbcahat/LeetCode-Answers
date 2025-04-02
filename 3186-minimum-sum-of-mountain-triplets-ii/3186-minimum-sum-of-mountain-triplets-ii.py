616bfd309a40278f3bfaf37d78cf405b121037d7
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-03 02:54:31 +0530

    Time: 167 ms (35.48%), Space: 32.9 MB (75.27%) - LeetHub

---
+++ 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumSum(self, nums: List[int]) -> int:
+
+
+        n = len(nums)
+        leftMin = [float("inf")]  * n 
+        rightMin = [float("inf")] * n 
+
+        for i in range(1, n): 
+            leftMin[i] = min(leftMin[i - 1], nums[i - 1])
+            rightMin[n - i - 1] = min(rightMin[n - i], nums[n - i])
+
+        res = float("inf")
+        for k in range(1, n - 1): 
+            if leftMin[k] < nums[k] and nums[k] > rightMin[k]: 
+                res = min(nums[k] + leftMin[k] + rightMin[k], res)
+
+        return -1 if res == float("inf") else res
