f035056df6bc3e3c7ba22248a467af595c6224d1
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-07 00:40:34 +0530

    Time: 0 ms (100%), Space: 18 MB (41.31%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        stack = []
+        track = defaultdict(int)
+        for i, n in enumerate(nums2): 
+            while stack and stack[-1] < n: 
+                elem = stack.pop()
+                track[elem] = i
+            stack.append(n)
+
+        for ind, n in enumerate(nums1): 
+            if n in track: 
+                nums1[ind] = nums2[track[n]]
+            else:
+                nums1[ind] = -1
+
+        return nums1
+        
