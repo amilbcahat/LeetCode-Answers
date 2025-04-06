69ded9e65f2fc2dfe1b1cfb5d011b4f0afc7f049
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-07 02:41:32 +0530

    Time: 127 ms (35.21%), Space: 33.3 MB (31.59%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,13 +2,11 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
         res = [0] * len(temperatures)
-        for i , t in enumerate(temperatures):
-            #Logic is simple  , append till you dont find a larger temperature 
-            #When you find , pop all , and calculate indice like shown below 
-            #return !
-            while stack and t > stack[-1][0]:
-                topTemp , topIndex = stack.pop()
-                res[topIndex] = (i - topIndex)
-            stack.append([t , i])
 
-        return res 
+        for i, t in enumerate(temperatures): 
+            while stack and stack[-1][0] < t: 
+                popTemp, popIndex = stack.pop()
+                res[popIndex] = (i - popIndex)
+            stack.append((t, i))
+
+        return res
