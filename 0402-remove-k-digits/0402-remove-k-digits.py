fb568596d9ea49a21f8dd33cbaffad728e9c0e22
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-08 05:08:20 +0530

    Time: 23 ms (48.44%), Space: 19.3 MB (9.22%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,15 +1,13 @@
 class Solution:
     def removeKdigits(self, nums: str, k: int) -> str:
-
+        #Using Monotonic Stack to remove the greater elements
         stack = []
         for i, n in enumerate(nums): 
             while stack and stack[-1] > n and k > 0: 
                 stack.pop()
                 k -= 1
             stack.append(n)
-        if len(stack) == 1 and k > 0: 
-            return "0"
-
+            
         p1 = -1 
         for i, n in enumerate(stack): 
             if n != "0": 
@@ -18,6 +16,6 @@ class Solution:
 
         st1 = "".join(stack[p1:])
 
-        #Now remember we used a stack, so its always increasing if , len of stack is same as len of string, nothing got deleted
+        #Now remember we used a stack, so its always increasing, the greatest is always at the right, now if k > 0, we can still go ahead and delete more 
         #So we will remove the right part, as it would be the greatest, and that would make the number smaller 
-        return st1 if not k else (st1[:-k] if st1[:-k] else "0")
+        return st1 if not k else (st1[:-k] if st1[:-k] else "0") #Nested ternary to handle the case if string is "" then return "0"
