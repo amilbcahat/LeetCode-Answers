2afd65aad10b14909b2fa073218a0c18d745f2c0
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-07 06:08:45 +0530

    Time: 2574 ms (8.54%), Space: 493.9 MB (5.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,47 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 : 
+        total = sum(nums)
+
+        if total % 2:
             return False 
 
-        target = sum(nums) // 2 
         dp = {}
-        #Easy way
-        def dfs(i , total) : 
-            if total == 0 : 
+        def dfs(i, curSum):
+            nonlocal total
+            if total // 2 == curSum:
                 return True 
 
-            if i >= len(nums) : 
-                return False 
+            if i >= len(nums):
+                return False
 
-            if (i , total) in dp:
-                return dp[(i , total)]
-            
-            #Include 
-            res = (dfs(i + 1 , total - nums[i]) or 
-            #Pass the subproblem (dont include)
-            dfs(i + 1 , total))
+            if (i, curSum) in dp:
+                return dp[(i, curSum)]
 
-            dp[(i , total)] = res
+            res = (dfs(i + 1, curSum + nums[i]) or dfs(i + 1, curSum))
 
+            dp[(i, curSum)] = res
             return res 
-        return dfs(0 , target)
-        # dp = set()
-        # #Base case -> We have a guaranteed 0 , if we dont want to choice any thing 
-        # dp.add(0)
-        # #Target is half of sum since we are partitioning into two halves ! 
-        # target = sum(nums) // 2
-        # for i in range(len(nums)-1,-1,-1):
-        #     #Making change in cloned dp because we are iterating on it 
-        #     nextDP = set(dp)
-        #     for t in dp : 
-        #         if (t+ nums[i]) == target : 
-        #             return True 
-        #         nextDP.add(t+nums[i])
-        #     dp = nextDP 
-        # #If found in dp then we can say that a half equal to target exists ! since half is target and we can reach that it confirms that both will equal to the primary input ! 
-        # return True if target in dp else False
 
-
-        
-
-        
+        return dfs(0, 0)
