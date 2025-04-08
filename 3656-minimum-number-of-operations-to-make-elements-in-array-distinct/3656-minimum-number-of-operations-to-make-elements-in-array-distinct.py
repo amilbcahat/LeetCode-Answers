628621b9a12a8970bf191dae77daf38c3b950bcc
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-08 12:52:34 +0530

    Time: 3 ms (69.84%), Space: 18.1 MB (9.05%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -3,17 +3,12 @@ class Solution:
         if len(nums) == len(set(nums)): 
             return 0
 
-        freqMap = Counter(nums)
         k = 0
         for i in range(len(nums)): 
-            # print(i, nums[i])
             if (i + 1) % 3 == 0: 
                 k += 1
-                # print(nums[i + 1:], set(nums[i + 1:]),i,len(set(nums[i + 1:])), len(nums) - i)
                 if len(set(nums[i + 1:])) == (len(nums) - i - 1): 
                     print(nums[i + 1:], len(nums) - i - 1)
                     return k 
-                
-
 
         return k + 1
