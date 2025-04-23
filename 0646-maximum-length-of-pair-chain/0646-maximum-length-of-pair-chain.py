7b222784b8d33c9a890c3d41ff0712a975fdadfc
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-24 04:17:15 +0530

    Time: 11 ms (52.7%), Space: 18 MB (81.37%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -9,7 +9,8 @@ class Solution:
             cur = pairs[i]
             if cur[0] <= prev[1]: 
                 res += 1
-                prev = [cur[0], min(prev[1], cur[1])]
+                prev = [cur[0] #prev[0] works fine too here
+                , min(prev[1], cur[1])]
             else:
                 prev = cur 
         return len(pairs) - res
