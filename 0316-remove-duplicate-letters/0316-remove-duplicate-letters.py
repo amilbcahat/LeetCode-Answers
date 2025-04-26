9cb3de3c2ad33003cd3217d0e79dfd56b0bee05f
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-27 00:45:30 +0530

    Time: 7 ms (11.97%), Space: 18 MB (26.99%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,12 +1,9 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        
-
         countMap = Counter(s)
         fix = set()
         stack = [s[0]]
         countMap[s[0]] -= 1
-        totalKeys = len(countMap.keys())
         for c in s[1:]: 
             if c not in fix: 
                 while stack and stack[-1] >= c and countMap[stack[-1]] > 0:
