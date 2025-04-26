196bef06c293c3bba7caef82650f458474191d4a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-26 07:06:59 +0530

    Time: 135 ms (73.63%), Space: 29 MB (16.39%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.py
@@ -1,25 +1,25 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        lastMinK = -1
-        lastMaxK = -1
+        lastMinK = -1 
+        lastMaxK = - 1
+        leftBound = -1
         res = 0 
-        culpritIndex = -1
-        # https://leetcode.com/problems/count-subarrays-with-fixed-bounds/solutions/4949239/counting-fixed-bound-subarrays-with-two-pointers/
-        for i in range(len(nums)):
-            if nums[i] > maxK or nums[i] < minK :
-                #Where existing subarray ends 
-                culpritIndex = i
 
-            if nums[i] == maxK :
-                lastMaxK = i
+        for i in range(len(nums)): 
+            if nums[i] > maxK or nums[i] < minK: 
+                #We can take this into our consideration 
+                #But from here we should calculate the combination of answers 
+                leftBound = i 
+
+            if nums[i] == maxK: 
+                lastMaxK = i 
             
-            if nums[i] == minK :
-                lastMinK = i
+            if nums[i] == minK:
+                lastMinK = i 
 
-            #start of valid subarray
-            startOfSubArray = min(lastMinK , lastMaxK)
-            #Length of valid subarray
-            temp = startOfSubArray - culpritIndex
-            res += temp if temp > 0 else 0 
+            startOfSubarray = min(lastMinK, lastMaxK)
+            temp = startOfSubarray - leftBound #Calculates combination wise 
 
-        return res 
+            res += temp if temp > 0 else 0 # leftBound will always be on left side of the minK, maxK
+
+        return res
