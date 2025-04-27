3f2cea65025392ebad2d371957060621f34ada8a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-27 20:35:49 +0530

    Time: 0 ms (100%), Space: 17.9 MB (86.99%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
@@ -5,20 +5,41 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None: 
-            return []
-        q = deque([root])
-        res = []
-        while q: 
-            level = []
-            for i in range(len(q)): 
-                node = q.popleft()
-                level.append(node.val)
-                if node and node.left: 
-                    q.append(node.left)
-                if node and node.right: 
-                    q.append(node.right)
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:    
+        levels = []
+        if not root: 
+            return levels
 
-            res.append(level)
-        return res[::-1]
+        def dfs(node, level): 
+            if len(levels) == level: 
+                levels.append([])
+
+            levels[level].append(node.val)
+
+            if node.left: 
+                dfs(node.left, level + 1)
+
+            if node.right: 
+                dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        return levels[::-1]
+
+        
+        # Through Level Order Travsersal
+        # if root is None: 
+        #     return []
+        # q = deque([root])
+        # res = []
+        # while q: 
+        #     level = []
+        #     for i in range(len(q)): 
+        #         node = q.popleft()
+        #         level.append(node.val)
+        #         if node and node.left: 
+        #             q.append(node.left)
+        #         if node and node.right: 
+        #             q.append(node.right)
+
+        #     res.append(level)
+        # return res[::-1]
