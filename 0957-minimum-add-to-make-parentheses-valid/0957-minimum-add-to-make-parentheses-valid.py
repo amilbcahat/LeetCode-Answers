74703cbe04faf967e7a42190fc159f6ac39cd5be
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-27 19:54:00 +0530

    Time: 0 ms (100%), Space: 17.8 MB (49.27%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -1,22 +1,13 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        openn = 0
-        close = 0 
-        neverClosed = 0
-        for b in s: 
-            if b == "(":
-                openn += 1
-            else: 
-                openn -= 1
+        left = right = 0 
+        for c in s : 
+            if c == "(": 
+                right += 1
+            elif c == ")": 
+                right -= 1 
+                if right == -1: 
+                    right = 0
+                    left += 1
 
-            if openn < 0: 
-                neverClosed = min(neverClosed, openn)
-
-            # print(openn)
-            
-        # print("open", openn, "neverClosed", neverClosed)
-        #When openn == neverClosed
-        #   Only never closed brackets would be there
-        #if open > neverClosed: 
-        #   There are -neverClosed brackets and open - neverClosed, open brackets
-        return (-neverClosed) + (openn - neverClosed)
+        return left + right 
