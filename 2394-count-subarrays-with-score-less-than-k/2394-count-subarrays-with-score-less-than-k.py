1ed865f175cafcf5ef4b7a9a220d5c22066f9977
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-04-28 20:46:37 +0530

    Time: 199 ms (8.58%), Space: 30.9 MB (14.3%) - LeetHub

---
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        
+        def atmost(k): 
+            l = 0 
+            total = 0
+            prevLen = 1
+            res = 0
+            for r in range(len(nums)): 
+                curLen = (r - l + 1)
+                total = total / (1 if prevLen == 0 else prevLen)
+                total = total * curLen + nums[r] * curLen
+
+                # print(nums[l: r + 1], "outside", total, curLen, prevLen)
+                while l <= r and total >= k: 
+                    total = total - nums[l] * curLen
+                    total = total / curLen 
+                    total = total * prevLen
+                    l += 1
+                    curLen -= 1
+                    prevLen -= 1
+                    # curLen = (r - l + 1)
+                    # prevLen = curLen
+                    # print(nums[l: r + 1], total, curLen, prevLen)
+
+                prevLen = curLen
+                res += curLen
+                # print(nums[l: r + 1], total)
+
+            return res
+
+        return atmost(k)
