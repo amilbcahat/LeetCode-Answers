557140aa76c4a66c8aec05b1e75676fa6094dec3
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-06 02:47:09 +0530

    Time: 0 ms (100%), Space: 18 MB (40.08%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
@@ -1,5 +1,14 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        #Follow the sequence 
+        # understand the sequence
+        # 5 = 2 * 2 + 1
+        # 11 = 5 * 2 + 1
+        # 24 = 11 * 2 + 2
+        # 53 = 24 * 2 + 5
+        # 117 = 53 * 2 + 11
+        # A[N] = A[N-1] * 2 + A[N-3]
+        # now try implementing
         first = 1 
         second = 2
         third = 5
