ac011157b051ea4a1c92b27438f9aed3db09e062
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-11 05:18:00 +0530

    Time: 872 ms (28.36%), Space: 35 MB (27.68%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -27,11 +27,9 @@ class Solution:
             if cntZero1 != 0 and cntZero2 != 0: 
                 ans = totalNums1 + moreZero
         elif totalNums1 + cntZero1 > totalNums2 + cntZero2: 
-            diff = (totalNums1 + cntZero1) - (totalNums2 + cntZero2) 
             if cntZero2 > 0: 
                 ans = totalNums1 + cntZero1 
         else: 
-            diff = (totalNums2 + cntZero2) - (totalNums1 + cntZero1)
             if cntZero1 > 0: 
                 ans = totalNums2 + cntZero2 
 
