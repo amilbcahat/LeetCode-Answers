01cb41359fa57cb8ab4aa600255cd9fabfdae550
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-11 05:30:56 +0530

    Time: 873 ms (26.68%), Space: 34.9 MB (42.45%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -1,38 +1,22 @@
 class Solution:
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
-        cntZero2 = 0 
-        totalNums2 = 0
-        for n in nums2: 
-            if n == 0: 
-                cntZero2 += 1
-            totalNums2 += n 
+        #Check Editorial, apt explanation
+        sum1 = sum2 = 0 
+        zero1 = zero2 = 0 
 
-
-        cntZero1 = 0 
-        totalNums1 = 0
         for n in nums1: 
+            sum1 += n
             if n == 0: 
-                cntZero1 += 1
-            totalNums1 += n 
+                zero1 += 1
+                sum1 += 1
 
-        
-        # print(totalNums1, totalNums2, cntZero1, cntZero2)
+        for n in nums2: 
+            sum2 += n
+            if n == 0: 
+                zero2 += 1
+                sum2 += 1
 
-        ans = -1
-        if totalNums1  == totalNums2  : 
-            if cntZero1 == 0 and cntZero2 == 0: 
-                return totalNums1
-            #Tie breaker on zero then 
-            moreZero = cntZero1 if cntZero1 > cntZero2 else cntZero2
-            if cntZero1 != 0 and cntZero2 != 0: 
-                ans = totalNums1 + moreZero
-        elif totalNums1 + cntZero1 > totalNums2 + cntZero2: 
-            if cntZero2 > 0: 
-                #Now we replace 0 to match 
-                ans = totalNums1 + cntZero1 
-        else: 
-            if cntZero1 > 0: 
-                #Now we replace 0 to match 
-                ans = totalNums2 + cntZero2 
+        if (zero1 == 0 and sum2 > sum1) or (zero2 == 0 and sum1 > sum2): 
+            return -1 
 
-        return ans
+        return max(sum1, sum2)
