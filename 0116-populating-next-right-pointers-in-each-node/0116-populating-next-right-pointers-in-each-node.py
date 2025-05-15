7cd239d94bde046756547cd3ffa878b3024b39a1
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-16 01:47:10 +0530

    Time: 49 ms (70.77%), Space: 19.2 MB (51.3%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,25 +10,39 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        q = deque([root])
+        cur, nxt = root, root.left if root else None 
 
-        while q: 
-            size = len(q)
-            for i in range(len(q)): 
-                node = q.popleft()
-                
-                if i < size - 1: 
-                # This check is important. We don't want to
-                # establish any wrong connections. The queue will
-                # contain nodes from 2 levels at most at any
-                # point in time. This check ensures we only
-                # don't establish next pointers beyond the end
-                # of a level
-                    node.next = q[0]
+        while cur and nxt: 
+            cur.left.next = cur.right 
 
-                if node and node.left : 
-                    q.append(node.left)
-                if node and node.right: 
-                    q.append(node.right)
+            if cur.next: 
+                cur.right.next = cur.next.left
 
+            cur = cur.next 
+            if not cur:
+                cur = nxt
+                nxt = cur.left
+        
         return root
+        # q = deque([root])
+
+        # while q: 
+        #     size = len(q)
+        #     for i in range(len(q)): 
+        #         node = q.popleft()
+                
+        #         if i < size - 1: 
+        #         # This check is important. We don't want to
+        #         # establish any wrong connections. The queue will
+        #         # contain nodes from 2 levels at most at any
+        #         # point in time. This check ensures we only
+        #         # don't establish next pointers beyond the end
+        #         # of a level
+        #             node.next = q[0]
+
+        #         if node and node.left : 
+        #             q.append(node.left)
+        #         if node and node.right: 
+        #             q.append(node.right)
+
+        # return root
