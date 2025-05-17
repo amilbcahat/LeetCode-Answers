3ce4b8d08db8d989339066b60c12066d24c6e40e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-17 21:02:42 +0530

    Time: 71 ms (98.24%), Space: 29.5 MB (21.84%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
@@ -1,13 +1,34 @@
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
-        nums.sort()
+        #Solution 1 (Counting Pattern from Grokking)
+        n = len(nums)
+        max_val = max(nums)
+        ans = 0
+        freq = [0] * (n + max_val)
 
-        #Problem 1827 
-        ans =  0 
-        for i in range(1, len(nums)): 
-            if nums[i - 1] >= nums[i]: 
-                diff = (nums[i - 1] - nums[i] + 1)
-                nums[i] += diff
-                ans += diff
+        for val in nums: 
+            freq[val] += 1
+
+        for i in range(len(freq)): 
+            if freq[i] <= 1: 
+                continue 
+
+            duplicates = freq[i] - 1
+            freq[i + 1] += duplicates
+            # freq[i] = 1
+            ans += duplicates
 
         return ans
+
+        # Solution 2 
+        # nums.sort()
+
+        # #Problem 1827 
+        # ans =  0 
+        # for i in range(1, len(nums)): 
+        #     if nums[i - 1] >= nums[i]: 
+        #         diff = (nums[i - 1] - nums[i] + 1)
+        #         nums[i] += diff
+        #         ans += diff
+
+        # return ans
