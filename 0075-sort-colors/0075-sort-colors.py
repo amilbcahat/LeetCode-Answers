a16f7ca3d4317f9852a670545387a67bf50148f9
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-17 14:10:25 +0530

    Time: 0 ms (100%), Space: 17.8 MB (53.7%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,19 +1,26 @@
 class Solution:
-    def sortColors(self, arr: List[int]) -> None:
+    def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Assuming arr only contains 0, 1 or 2
-        counts = [0, 0, 0]
+        l , r = 0 , len(nums)-1 
+        i = 0 
+        def swap(i, j): 
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp
+       #We use two pointers l and r , l moves when we find a 0 and it is swapped with left array hence maintaining a number 0f zeroes in start 
+       #r is used to keep track of last element of 2's and is moved when i finds a 2 and is swapped 
+       #But we dont increment i , incase we find a 2 , because then it messes up the 0 order 
+       #Since 0 and 2 are sorted , 1 automatically resides in the middle ! 
 
-        # Count the quantity of each val in arr
-        for n in arr:
-            counts[n] += 1
-        
-        # Fill each bucket in the original array
-        i = 0
-        for n in range(len(counts)):
-            for j in range(counts[n]):
-                arr[i] = n
-                i += 1
-        return arr
+        while i <= r: 
+            if nums[i] == 0: 
+                swap(l, i)
+                l += 1 
+            if nums[i] == 2:
+                swap(i, r)
+                r -= 1
+                #I is decremented to cancel out the increment outside , and so displacement is 0 of i , we increment it outside , because then while loop can go in a state , infinite loop
+                i -= 1
+            i += 1
