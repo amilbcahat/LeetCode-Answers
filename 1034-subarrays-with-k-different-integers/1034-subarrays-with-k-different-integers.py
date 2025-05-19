50c28ad840d06b816e910da59c888a2ad38342a5
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-20 01:51:41 +0530

    Time: 87 ms (91.09%), Space: 20.5 MB (5.18%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.py
@@ -1,28 +1,56 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        #One Pass 
+        count = defaultdict(int)
+        total = 0 
+        left = 0 
+        right = 0 
+        currentWin = 0
 
-        def atmost(k): 
-            l = 0 
-            res = 0 
-            countMap = defaultdict(int)
-            distinct = set()
-            for r in range(len(nums)):
-                elem = nums[r]
-                countMap[elem] += 1
-                distinct.add(elem)
-                # print(distinct, l)
-                while l <= r and len(distinct) > k : 
-                    # print(nums[l : r + 1])
-                    countMap[nums[l]] -= 1 
-                    if countMap[nums[l]] == 0 : 
-                        distinct.remove(nums[l])
-                    l += 1 
+        while right < len(nums): 
+            if count[nums[right]] == 0: 
+                k -= 1
+            
+            count[nums[right]] += 1
+            right += 1
 
-                # print(nums[l : r + 1])
-                res += (r - l + 1)
+            if k < 0: 
+                count[nums[left]] -= 1
+                if count[nums[left]] == 0: 
+                    k += 1
+                left += 1
+                currentWin = 0
 
-            return res 
+            if k == 0: 
+                while count[nums[left]] > 1: 
+                    count[nums[left]] -= 1
+                    left += 1
+                    currentWin += 1
 
-        #Count of SubArrays with K Distinct Elements = Count of SubArrays with At Most K Distinct Elements - Count of SubArrays with At Most K-1 Distinct Elements
-        return (atmost(k) - atmost(k - 1))
-        # return (atmost(k) - atmost(k - 1))
+                total += (currentWin + 1)
+
+        return total
+
+
+        #Two Pass         
+        # def atmost(k): 
+        #     l = 0 
+        #     res = 0 
+        #     distinct = set()
+        #     countMap = defaultdict(int)
+
+        #     for r in range(len(nums)):
+        #         distinct.add(nums[r])
+        #         countMap[nums[r]] += 1 
+
+        #         while l <= r and len(distinct) > k : 
+        #             countMap[nums[l]] -= 1 
+        #             if countMap[nums[l]] == 0 : 
+        #                 distinct.remove(nums[l])
+        #             l += 1
+
+        #         res += (r - l + 1)
+
+        #     return res 
+
+        # return atmost(k) - atmost(k - 1)
