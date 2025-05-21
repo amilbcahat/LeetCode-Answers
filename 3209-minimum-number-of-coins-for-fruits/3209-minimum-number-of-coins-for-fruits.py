b8c6aa39e4f9a7b349e9d86d29746d561c11901e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-05-22 01:48:14 +0530

    Time: 8 ms (79.05%), Space: 18 MB (58.78%) - LeetHub

--- 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.py
+++ 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.py
@@ -1,15 +1,29 @@
 class Solution:
     def minimumCoins(self, prices: List[int]) -> int:
-            
-        @cache
-        def dfs(i): 
-            if i >= len(prices): 
-                return 0
+        #Like Bottom Up Solution
+        n = len(prices)
+        deque_ = deque()
+        deque_.append(n)
+        prices = prices + [0]  # Add a dummy fruit with 0 coins to avoid boundary issues.
 
-            ans = float("inf")
-            for next_pur in range(i + 2): 
-                ans = min(ans, prices[i] + dfs(i + next_pur + 1))
+        # Iterate from the last fruit to the first
+        for i in range(n - 1, -1, -1):
+            # Calculate the maximum index that can be covered by the current fruit
+            maxCoveredIndex = min(n, 2 * i + 2)
 
-            return ans
+            # Remove all elements from the front that are outside the range of current fruit's reward
+            while deque_ and deque_[0] > maxCoveredIndex:
+                deque_.popleft()
 
-        return dfs(0)
+            # Update the cost of the current fruit by adding the minimum cost of reachable fruits
+            prices[i] += prices[deque_[0]]
+
+            # Maintain the deque to keep it increasing
+            while deque_ and prices[i] < prices[deque_[-1]]:
+                deque_.pop()
+
+            # Add the current fruit index to the deque
+            deque_.append(i)
+
+        # Return the minimum cost to get all fruits starting from the first one
+        return prices[0]
