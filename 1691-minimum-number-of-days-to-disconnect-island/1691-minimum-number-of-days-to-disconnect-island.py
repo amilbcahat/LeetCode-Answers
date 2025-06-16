d3cdc7454b6e7fda0198d40a0c659c7fccddfddb
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-06-17 03:57:11 +0530

    Time: 15 ms (94.52%), Space: 18.3 MB (52.05%) - LeetHub

--- 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.py
+++ 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.py
@@ -1,47 +1,75 @@
 class Solution:
     def minDays(self, grid: List[List[int]]) -> int:
-        ROWS , COLS = len(grid) , len(grid[0])
+        rows, cols = len(grid), len(grid[0])
 
-        def dfs(r , c , visit): 
-            if (r < 0 or c < 0 or c == COLS or r == ROWS or grid[r][c] == 0 
-            or (r , c) in visit) : 
-                return 
+        ap_info = {"has_articulation": False, "time": 0}
 
-            visit.add((r , c))
-            dfs(r + 1 , c, visit)
-            dfs(r - 1, c, visit)
-            dfs(r , c + 1, visit)
-            dfs(r , c - 1, visit)
+        parents = [[-1] * cols for _ in range(rows)]
+        low_reachable = [[-1] * cols for _ in range(rows)]
+        discovery_time = [[-1] * cols for _ in range(rows)]
 
+        di = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
+        def find_articulation_point(row, col): 
+            discovery_time[row][col] = ap_info["time"]
+            ap_info["time"] += 1
+            low_reachable[row][col] = discovery_time[row][col]
 
-        def count_islands():
-            visit = set()
-            count = 0 
-            for r in range(ROWS):
-                for c in range(COLS):
-                    if grid[r][c] == 1 and (r , c) not in visit : 
-                        dfs(r , c , visit)
-                        count += 1
+            children = 0 
 
-            return count 
+            for r , c in di: 
+                new_col = c + col
+                new_row = r + row 
 
-        #Remove no land and count , see if result hits
-        if count_islands() != 1 : 
+                if (
+                    0 <= new_row < rows and 
+                    0 <= new_col < cols and 
+                    grid[new_row][new_col] == 1
+                ): 
+                    if discovery_time[new_row][new_col] == -1: 
+                        children += 1
+                        parents[new_row][new_col] = (
+                            row * cols + col
+                        )
+
+                        find_articulation_point(new_row, new_col)
+
+                        low_reachable[row][col] = min(
+                            low_reachable[row][col], 
+                            low_reachable[new_row][new_col]
+                        )
+
+                        if low_reachable[new_row][new_col] >= discovery_time[row][col] and parents[row][col] != -1: 
+                            ap_info["has_articulation"] = True 
+
+                    elif new_row * cols + new_col != parents[row][col]: 
+                        low_reachable[row][col] = min(
+                            low_reachable[row][col], 
+                            discovery_time[new_row][new_col]
+                        )
+                    #backedge
+
+            if parents[row][col] == -1 and children > 1: 
+                ap_info["has_articulation"] = True
+
+        islands = 0 
+        lands = 0 
+        for i in range(rows): 
+            for j in range(cols): 
+                if grid[i][j] == 1: 
+                    lands += 1
+                    if discovery_time[i][j] == -1:
+                        find_articulation_point(i, j)
+                        islands += 1
+
+        if islands == 0 or islands >= 2:
             return 0 
 
-        #Remove 1 land and count , see if result hits 
+        if lands == 1: 
+            return 1 
 
-        for r in range(ROWS) : 
-            for c in range(COLS) : 
-                if grid[r][c] == 0 : 
-                    continue 
-                grid[r][c] = 0 
-                if count_islands() != 1 : 
-                    return 1
-                grid[r][c] = 1
-
-        #If both remove cases , dont work , then definitely , its 2 removals 
+        if ap_info["has_articulation"]: 
+            return 1
 
         return 2 
-        
+
