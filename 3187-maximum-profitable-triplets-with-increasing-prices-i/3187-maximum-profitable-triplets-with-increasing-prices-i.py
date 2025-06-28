f1300767d8b8ebb5e56c3d660a6a0edf28e5502e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-06-28 20:39:30 +0530

    Time: 459 ms (83.33%), Space: 33.4 MB (16.67%) - LeetHub

--- 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
+++ 3187-maximum-profitable-triplets-with-increasing-prices-i/3187-maximum-profitable-triplets-with-increasing-prices-i.py
@@ -1,38 +1,50 @@
 class Solution:
-    def getMaxProfit(self, bit, priceIndex):
-        i = priceIndex 
-        res = 0
-        while i > 0: 
-            res = max(bit[i], res)
-            i -= i & -i
-        return res
+    def maxProfit(self, prices, profits):
+        # Find the maximum price from the prices array
+        maxPrice = 0
+        n = len(prices)
+        for price in prices:
+            maxPrice = max(maxPrice, price)
 
-    def updateProfit(self, bit, priceIndex, profit):
-        i = priceIndex 
-        while i > 0 and i < len(bit): 
+        # Initialize Binary Indexed Trees (BIT) for left and right max profits
+        bitForMaxLeft = [0] * (maxPrice + 1)
+        bitForMaxRight = [0] * (maxPrice + 1)
+
+        # Array to store max profit for the left side of each price
+        maxLeftProfit = [0] * n
+
+        # Calculate maxLeftProfit using BIT for each price
+        for i in range(n):
+            maxLeftProfit[i] = self.getMaxProfit(bitForMaxLeft, prices[i] - 1)
+            self.updateBIT(bitForMaxLeft, prices[i], profits[i])
+
+        # Initialize the result as -1 indicating no valid triplet found yet
+        maxTotalProfit = -1
+
+        # Traverse the prices array from right to left to calculate the result
+        for i in range(n - 1, -1, -1):
+            adjustedPrice = maxPrice - prices[i] + 1
+            maxRightProfit = self.getMaxProfit(bitForMaxRight, adjustedPrice - 1)
+
+            # Update maxTotalProfit if both maxLeftProfit and maxRightProfit are greater than 0
+            if maxLeftProfit[i] > 0 and maxRightProfit > 0:
+                maxTotalProfit = max(maxTotalProfit, maxLeftProfit[i] + profits[i] + maxRightProfit)
+            self.updateBIT(bitForMaxRight, adjustedPrice, profits[i])
+
+        return maxTotalProfit
+
+    # Update BIT with the new profit value at the given price index
+    def updateBIT(self, bit, priceIndex, profit):
+        i = priceIndex
+        while i > 0 and i < len(bit):
             bit[i] = max(bit[i], profit)
             i += i & -i
 
-    def maxProfit(self, prices: List[int], profits: List[int]) -> int:
-        n = len(prices)
-        maxPrice = max(prices)
-        bitMaxLeft = [0] * (maxPrice + 1)
-        bitMaxRight = [0] * (maxPrice + 1)
-        maxLeftProfit = [0] * n
-        for i in range(n): 
-            maxLeftProfit[i] = self.getMaxProfit(bitMaxLeft, prices[i] - 1)
-            self.updateProfit(bitMaxLeft, prices[i], profits[i])
-
-        maxTotalprofit = -1 
-
-        for i in range(n - 1, -1 , -1):
-
-            adjustedPrice = maxPrice - prices[i] + 1
-            maxRightProfit = self.getMaxProfit(bitMaxRight, adjustedPrice - 1)
-
-            if maxLeftProfit[i] > 0 and maxRightProfit > 0: 
-                maxTotalprofit = max(maxTotalprofit, maxLeftProfit[i] + profits[i] + maxRightProfit)
-            
-            self.updateProfit(bitMaxRight, adjustedPrice, profits[i])
-
-        return maxTotalprofit
+    # Get the maximum profit value from BIT for a given price index
+    def getMaxProfit(self, bit, priceIndex):
+        maxProfit = 0
+        i = priceIndex
+        while i > 0:
+            maxProfit = max(bit[i], maxProfit)
+            i -= i & -i
+        return maxProfit
