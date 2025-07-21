19664580153d11db51841bb2f1825b10ad858df4
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-07-21 21:10:40 +0530

    Time: 0 ms (100%), Space: 18 MB (30.98%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,18 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def helper(x,n):
-            #x raised to any power is zero 
-            if x == 0 : 
-                return 0 
-            #x raised to 0 is 1 
-            if n == 0 : 
-                return 1 
-            #n//2 is taken because 2^10 can be writtern as 2^5*2^5 , hence a divide and conquer approach
-            res = helper(x,n//2)
-            #2^10 = 2^5 * 2^5, thats why 
-            res = res * res 
-            #We return x * res if power is odd because 2^5 = 2 * 2^2 * 2^2
-            return x * res if n % 2 else res 
-        res = helper(x , abs(n))
-        #else condition is for handling negative power ! 
-        return res if n>=0 else 1/res
+        def poww(x, n):
+            if n == 0: 
+                return 1
+            res = self.myPow(x, n // 2)
+            if(n & 1): #odd
+                return x * res * res 
+            else: 
+                return res * res
+
+        return poww(x, abs(n)) if n > 0 else 1 / poww(x, abs(n)) 
