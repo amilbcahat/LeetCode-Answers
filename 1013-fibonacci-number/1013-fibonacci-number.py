6f866f607b1664ebd810d96d8349c437b6cdce4a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-07-22 09:38:04 +0530

    Time: 357 ms (20.26%), Space: 17.8 MB (23.22%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,31 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
-        #Constant space 
-        p1 = 1 
-        p2 = 0 
+        if n == 0: 
+            return 0 
 
-        tmp = 0 
-        for i in range(n):
-            tmp = p1 + p2 
-            p1 = p2 
-            p2 = tmp
+        if n == 1: 
+            return 1 
 
-        return tmp
-
-        
-        # dp = {}
-        # def dfs(n) : 
-        #     if n in dp : 
-        #         return dp[n]
-            
-        #     if n == 0 : 
-        #         return 0 
-
-        #     if n == 1: 
-        #         return 1
-
-        #     dp[n] = dfs(n - 1) + dfs(n - 2) 
-
-        #     return dp[n]
-
-        # return dfs(n)
+        return self.fib(n - 2) + self.fib(n - 1)
