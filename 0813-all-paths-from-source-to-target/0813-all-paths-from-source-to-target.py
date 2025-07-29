0e287f3634336fdf7eeae83b9f87993f8b6149cf
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-07-30 03:55:28 +0530

    Time: 7 ms (42.26%), Space: 20.8 MB (10.77%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -2,23 +2,47 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         #Editorial please check always
         # This will help in understanding that - https://math.stackexchange.com/a/3961816
-        n = len(graph)
-        res = []
-        def dfs(node, path):
-            nonlocal res 
-            if node == n - 1: 
-                res.append(list(path))
-                return
+        # Backtracking
+        # n = len(graph)
+        # res = []
+        # def dfs(node, path):
+        #     nonlocal res 
+        #     if node == n - 1: 
+        #         res.append(list(path))
+        #         return
 
+        #     for nei in graph[node]: 
+        #         path.append(nei)
+        #         dfs(nei, path) 
+        #         path.pop()
+
+        # dfs(0, [0])
+
+        # return res
+
+        #Top Down Approach 
+        # allPathsToTarget(currNode)={currNode+allPathsToTarget(nextNode)}
+        target = len(graph) - 1
+
+        @lru_cache(maxsize=None)
+        def all_paths_to_target(node): 
+            if node == target: 
+                return [[target]]
+
+            path_to_target_from_this_node = []
             for nei in graph[node]: 
-                path.append(nei)
-                dfs(nei, path) 
-                path.pop()
+                for path in all_paths_to_target(nei): 
+                    path_to_target_from_this_node.append([node] + path)
+            
+            return path_to_target_from_this_node
 
-        dfs(0, [0])
+        return all_paths_to_target(0)
 
-        return res
+
+
+
 
 
 
         
+6
