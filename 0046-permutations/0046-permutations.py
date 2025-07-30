7ad32d30e9a99df64c5981e6975ea207897048f4
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-07-31 03:12:17 +0530

    Time: 0 ms (100%), Space: 18.1 MB (15.92%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0: 
+            return [[]]
 
-        #Neetcode Video 
-        perms = [[]]
-        #[[1]]
-        #[[1 , 2] , [2 , 1]] Add 2 , to every index of both 
-        # Now add 3 at every index of both 
-        for n in nums : 
-            new_perms = []
-            for p in perms : 
-                for i in range(len(p) + 1):
-                    p_copy = p.copy()
-                    p_copy.insert(i , n)
-                    new_perms.append(p_copy)
-            perms = new_perms 
-        return perms 
+
+        perms = self.permute(nums[1:])
+        res = []
+        for p in perms: 
+            for i in range(len(p) + 1): 
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+
+        return res
