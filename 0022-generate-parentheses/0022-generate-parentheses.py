96c29d3f84e8d4e3aa02ea9a87c9cddcafda765d
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-07-31 02:39:24 +0530

    Time: 0 ms (100%), Space: 18.1 MB (43.4%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,21 +2,43 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         #Why not stack ? 
         #Stack can only confirm , for generating we need bactrack 
-        #Point 1: start with open 
+        # backtrack 
         res = []
-        def backtrack(path, openn): 
-            if openn < 0: 
-                return 
-
+        def backtrack(path, left_count, right_count): 
             if len(path) == n * 2: 
-                if openn == 0:
-                    res.append(path) 
+                res.append("".join(path))
                 return 
 
-            backtrack(path + "(", openn + 1)
-            backtrack(path + ")", openn - 1)
+            if left_count < n: 
+                path.append("(")
+                backtrack(path, left_count + 1, right_count) 
+                path.pop()
 
-        backtrack("(", 1)
+            if left_count > right_count: 
+                path.append(")")
+                backtrack(path, left_count, right_count + 1)
+                path.pop()
+
+        backtrack([], 0, 0)
+
         return res
+
+        #Point 1: start with open , recursion 
+        # res = []
+        # def backtrack(path, openn): 
+        #     if openn < 0: 
+        #         #closed bracket has some point dominated open, thats why
+        #         return 
+
+        #     if len(path) == n * 2: 
+        #         if openn == 0:
+        #             res.append(path) 
+        #         return 
+
+        #     backtrack(path + "(", openn + 1)
+        #     backtrack(path + ")", openn - 1)
+
+        # backtrack("(", 1)
+        # return res
 
 
