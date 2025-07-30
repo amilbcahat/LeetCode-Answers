99d22eea7c17184d00c203893769c1f0f1a6beb2
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-07-31 03:17:27 +0530

    Time: 0 ms (100%), Space: 18.2 MB (15.92%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,29 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0: 
-            return [[]]
 
+        #iterative
+        perms = [[]]
+        for n in nums: 
+            new_perms = []
+            for p in perms: 
+                for i in range(len(p) + 1): 
+                    p_copy = p.copy()
+                    p_copy.insert(i, n)
+                    new_perms.append(p_copy)
 
-        perms = self.permute(nums[1:])
-        res = []
-        for p in perms: 
-            for i in range(len(p) + 1): 
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                res.append(p_copy)
+            perms = new_perms
+        return perms
 
-        return res
+        # Recursion
+        # if len(nums) == 0: 
+        #     return [[]]
+
+        # perms = self.permute(nums[1:])
+        # res = []
+        # for p in perms: 
+        #     for i in range(len(p) + 1): 
+        #         p_copy = p.copy()
+        #         p_copy.insert(i, nums[0])
+        #         res.append(p_copy)
+
+        # return res
