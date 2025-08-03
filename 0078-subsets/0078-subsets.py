920dd30ee6abe38e4c8a7e76ea2927c4d02d4d17
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-04 04:44:17 +0530

    Time: 0 ms (100%), Space: 17.8 MB (65.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,27 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        #subsets https://leetcode.com/problems/subsets/solutions/5190785/python-bit-manipulation-code-walkthrough-step-by-step-with-comments
         n = len(nums)
-        res = []
-        def dfs(i, cur):
-            res.append(cur[:])
+        ans = []
+        for num in range(1 << n): 
+            subsets = []
+            for i in range(n): 
+                if (num & (1 << i)) != 0: 
+                    subsets.append(nums[i])
+            ans.append(subsets)
 
-            for j in range(i, n):
-                cur.append(nums[j])
-                dfs(j + 1, cur)
-                cur.pop()
+        return ans
 
-        dfs(0, [])
-        return res
+        # n = len(nums)
+        # res = []
+        # def dfs(i, cur):
+        #     res.append(cur[:])
+
+        #     for j in range(i, n):
+        #         cur.append(nums[j])
+        #         dfs(j + 1, cur)
+        #         cur.pop()
+
+        # dfs(0, [])
+        # return res
             
