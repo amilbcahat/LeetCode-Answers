f32fd3e1369b45c5454993167e5a56340a30f0c7
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-11 00:14:56 +0530

    Time: 27 ms (87.72%), Space: 29.6 MB (68.21%) - LeetHub

--- 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.py
+++ 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.py
@@ -1,6 +1,6 @@
 class Solution:
     def sortPermutation(self, nums: List[int]) -> int:
-        
+        # Solution: https://leetcode.com/problems/maximum-k-to-sort-a-permutation/solutions/7062883/no-cycle-just-simple-cumulative-and/
         mask = (1 << 30) - 1
 
         for i in range(len(nums)):
