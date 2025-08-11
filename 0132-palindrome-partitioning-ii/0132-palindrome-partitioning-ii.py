be0275d495c67bde3ae6818429906fe79c4cc063
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-12 01:37:33 +0530

    Time: 3118 ms (13.95%), Space: 501.2 MB (5.08%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -1,41 +1,39 @@
 class Solution:
-    def __init__(self):
-        self.memo_cuts = []
-        self.memo_palindrome = []
-
     def minCut(self, s: str) -> int:
-        self.memo_cuts = [None] * len(s)
-        self.memo_palindrome = [[None] * len(s) for _ in range(len(s))]
-        return self.find_minimum_cut(s, 0, len(s) - 1, len(s) - 1)
+        
+        memoPali = {}
+        def isValidPali(start, end):
+            if start >= end: 
+                return True 
 
-    def find_minimum_cut(
-        self, s: str, start: int, end: int, minimum_cut: int
-    ) -> int:
-        # base case
-        if start == end or self.is_palindrome(s, start, end):
-            return 0
-        # check for results in memo_cuts
-        if self.memo_cuts[start] is not None:
-            return self.memo_cuts[start]
-        for current_end_index in range(start, end + 1):
-            if self.is_palindrome(s, start, current_end_index):
-                minimum_cut = min(
-                    minimum_cut,
-                    1
-                    + self.find_minimum_cut(
-                        s, current_end_index + 1, end, minimum_cut
-                    ),
-                )
-        self.memo_cuts[start] = minimum_cut
-        return minimum_cut
+            if s[start] != s[end]: 
+                return False
 
-    def is_palindrome(self, s: str, start: int, end: int) -> bool:
-        if start >= end:
-            return True
-        # check for results in memo_palindrome
-        if self.memo_palindrome[start][end] is not None:
-            return self.memo_palindrome[start][end]
-        self.memo_palindrome[start][end] = s[start] == s[
-            end
-        ] and self.is_palindrome(s, start + 1, end - 1)
-        return self.memo_palindrome[start][end]
+            if (start, end) in memoPali: 
+                return memoPali[(start, end)]
+
+            memoPali[(start, end)] = isValidPali(start + 1, end - 1)
+
+            return memoPali[(start, end)]
+
+
+
+        memo = {}
+        def backtrack(i): 
+            if i >= len(s):
+                return -1
+
+            if i in memo: 
+                return memo[i]
+
+            res = float("inf")
+            for j in range(i, len(s)):                         
+                if isValidPali(i, j):                   
+                    res = min(res, 1 + backtrack(j + 1))
+
+            memo[i] = res
+
+            return res
+
+        return backtrack(0)
+        
