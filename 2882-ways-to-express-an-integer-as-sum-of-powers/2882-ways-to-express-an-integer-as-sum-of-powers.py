d4624e013a434f1e8af19d617852101faef88e80
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-14 02:46:34 +0530

    Time: 2347 ms (11.63%), Space: 19.7 MB (40.47%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        # cacheTwo = {}
+        # def pow(x, n): 
+        #     if n == 0: 
+        #         return 1 
+            
+        #     if (x, n) in cacheTwo:
+        #         return cacheTwo[(x, n)]
+
+        #     res = pow(x, n // 2)
+        #     ans = -1
+        #     if n & 1: 
+        #         ans = x * res * res
+        #     else: 
+        #         ans = res * res
+        #     cacheTwo[(x, n)] = ans
+        #     return ans
+
+        MOD = 10 ** 9 + 7 
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+
+        for i in range(1, n + 1): 
+            val = i ** x 
+            for j in range(n + 1): 
+                dp[i][j] = dp[i - 1][j]
+                if j >= val: 
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % MOD
+
+        return dp[n][n]
