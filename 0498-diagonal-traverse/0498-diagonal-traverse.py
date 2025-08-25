fcd125eec75ed8a7ccc7928e7e68ba2094317cfc
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-25 14:25:24 +0530

    Time: 7 ms (85.22%), Space: 19.7 MB (86.08%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,28 +1,51 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        #Editorial Solution 1- 
+        N, M = len(mat), len(mat[0])
+        res, intermediate = [], []
+
+        for d in range(N + M - 1): 
+            intermediate.clear()
+
+            r, c = 0 if d < M else d - M + 1, d if d < M else M - 1
+
+            while r < N and c > -1: 
+                intermediate.append(mat[r][c])
+                r += 1
+                c -= 1
+
+            if d % 2 == 0: 
+                res.extend(intermediate[::-1])
+            else:
+                res.extend(intermediate)
+
+        return res
+
+
+        #My solution - 
         #check n queens ques 
         #intuition is that , pos diag elements in same diag are in r + c
-        m, n = len(mat), len(mat[0])
-        last = (m - 1) + (n - 1)
-        diagMap = defaultdict(list)
-        ans = [-1] * (m * n)
-        for r in range(m): 
-            for c in range(n): 
-                if (r + c) & 1 == 0:     
-                    diagMap[r + c].append(mat[r][c])
-                else: 
-                    diagMap[r + c].append(mat[r][c])
+        # m, n = len(mat), len(mat[0])
+        # last = (m - 1) + (n - 1)
+        # diagMap = defaultdict(list)
+        # ans = [-1] * (m * n)
+        # for r in range(m): 
+        #     for c in range(n): 
+        #         if (r + c) & 1 == 0:     
+        #             diagMap[r + c].append(mat[r][c])
+        #         else: 
+        #             diagMap[r + c].append(mat[r][c])
 
-        idx = 0 
-        for itemS, val in diagMap.items():
-            if itemS & 1 == 0: 
-                for i in range(len(val) - 1, -1 , -1): 
-                    ans[idx] = val[i]
-                    idx += 1
-            else:
-                for i in range(len(val)):
-                    ans[idx] = val[i]
-                    idx += 1
+        # idx = 0 
+        # for itemS, val in diagMap.items():
+        #     if itemS & 1 == 0: 
+        #         for i in range(len(val) - 1, -1 , -1): 
+        #             ans[idx] = val[i]
+        #             idx += 1
+        #     else:
+        #         for i in range(len(val)):
+        #             ans[idx] = val[i]
+        #             idx += 1
 
-        return ans
+        # return ans
             
