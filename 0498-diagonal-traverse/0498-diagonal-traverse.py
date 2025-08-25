72d3cb6d3ced8a7c11c4a2f439a0761f97d20aaf
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-25 13:45:00 +0530

    Time: 22 ms (18.77%), Space: 20.9 MB (21.18%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        last = (m - 1) + (n - 1)
+        diagMap = defaultdict(list)
+        ans = [-1] * (m * n)
+        for r in range(m): 
+            for c in range(n): 
+                if (r + c) & 1 == 0:     
+                    diagMap[r + c].append(mat[r][c])
+                else: 
+                    diagMap[r + c].append(mat[r][c])
+
+        idx = 0 
+        for itemS, val in diagMap.items():
+            if itemS & 1 == 0: 
+                for i in range(len(val) - 1, -1 , -1): 
+                    ans[idx] = val[i]
+                    idx += 1
+            else:
+                for i in range(len(val)):
+                    ans[idx] = val[i]
+                    idx += 1
+
+        return ans
+            
