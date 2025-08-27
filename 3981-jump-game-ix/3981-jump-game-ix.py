899e80fc0bf2bdf0325f87cce47e504384277b21
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-27 19:22:12 +0530

    Time: 344 ms (58.05%), Space: 33.8 MB (92.63%) - LeetHub

---
+++ 3981-jump-game-ix/3981-jump-game-ix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxValue(self, nums: List[int]) -> List[int]:
+        #Solution Description here
+        n = len(nums)
+        pre = [-1] * n 
+        suff = [-1] * n 
+        res = [-1] * n 
+
+        pre[0] = nums[0]
+        for i in range(1, n): 
+            #Here we can see , nums[i] is here, it means 
+            #this itself is checking if we can go left or no
+            pre[i] = max(nums[i], pre[i - 1])
+
+        suff[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1 , -1): 
+            suff[i] = min(nums[i], suff[i + 1])
+
+        res[n - 1] = pre[n - 1] # base case, we can reach max from here for sure
+        for i in range(n - 2, -1, -1): 
+            res[i] = pre[i]
+            if pre[i] > suff[i + 1]: 
+                #if we can get a higher pre (itself this i is also considered, then we can just get the highest max reachable from the suff side, so we do res[i] = res[i + 1])
+                res[i] = res[i + 1]
+
+        return res
