28c2b76065f2418f263e913f4f5596188e109315
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-29 02:22:07 +0530

    Time: 14 ms (15.63%), Space: 21.2 MB (89.87%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,23 +1,33 @@
 class MinStack:
 
     def __init__(self):
-        self.st = []
+        self.min_stack = []
+        self.stack = []
 
     def push(self, val: int) -> None:
-        self.st.append((val, min(val, self.st[-1][1] if self.st else val)))
-        
+        self.stack.append(val)
+        if not self.min_stack or val < self.min_stack[-1][0]: 
+            self.min_stack.append([val , 1])
 
+        elif val == self.min_stack[-1][0]: 
+            self.min_stack[-1][1] += 1
+
+        
     def pop(self) -> None:
-        self.st.pop()
+        if self.stack[-1] == self.min_stack[-1][0]: 
+            self.min_stack[-1][1] -= 1
+
+        if self.min_stack[-1][1] == 0: 
+            self.min_stack.pop()
+        
+        self.stack.pop()
         
 
     def top(self) -> int:
-        return self.st[-1][0]
-        
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.st[-1][1]
-
+        return self.min_stack[-1][0]
         
 
 
