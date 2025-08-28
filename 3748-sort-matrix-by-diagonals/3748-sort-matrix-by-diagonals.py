579905c18cad7b58943e8329f080dd3b2fdcfef1
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-28 20:58:28 +0530

    Time: 32 ms (8.33%), Space: 18.2 MB (9.17%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -0,0 +1,50 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid) - 1
+
+        heads = []
+
+        for d in range(n + 1): 
+            minHeap = []
+            #increasing
+            if True: 
+                #head 1 (0, d) 
+                r, c = d, 0
+                while r <= n  and c <= n :
+                    print(r, c)
+                    heapq.heappush(minHeap, grid[r][c])
+                    r += 1
+                    c += 1                
+
+                c = (n) - d
+                r = n
+                print("next start",r, c ,"head", d, 0)
+                
+                while minHeap and r >=  0 and c >= 0: 
+                    val = heapq.heappop(minHeap)
+                    grid[r][c] = val
+                    r -= 1
+                    c -= 1
+
+                minHeap.clear()
+                #head 1 (0, d) 
+                r, c = 0 , d 
+                if c > 0:
+                    while r <= n  and c <= n :
+                        heapq.heappush(minHeap, grid[r][c])
+
+                        r += 1
+                        c += 1
+                    
+                    r , c = 0, d
+                    while minHeap and r <= n  and c <= n: 
+                        val = heapq.heappop(minHeap)
+                        grid[r][c] = val
+                        r += 1
+                        c += 1
+
+                    minHeap.clear()
+
+
+
+        return grid
