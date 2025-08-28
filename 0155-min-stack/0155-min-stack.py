f1f18cff24d4f988174175a1d8c3feff4f61e28c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-29 02:23:08 +0530

    Time: 7 ms (51.55%), Space: 21.3 MB (89.87%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,39 +1,41 @@
 class MinStack:
 
     def __init__(self):
-        self.min_stack = []
         self.stack = []
+        self.min_stack = []
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.min_stack or val < self.min_stack[-1][0]: 
-            self.min_stack.append([val , 1])
+    def push(self, x: int) -> None:
 
-        elif val == self.min_stack[-1][0]: 
+        # We always put the number onto the main stack.
+        self.stack.append(x)
+
+        # If the min stack is empty, or this number is smaller than
+        # the top of the min stack, put it on with a count of 1.
+        if not self.min_stack or x < self.min_stack[-1][0]:
+            self.min_stack.append([x, 1])
+
+        # Else if this number is equal to what's currently at the top
+        # of the min stack, then increment the count at the top by 1.
+        elif x == self.min_stack[-1][0]:
             self.min_stack[-1][1] += 1
 
-        
     def pop(self) -> None:
-        if self.stack[-1] == self.min_stack[-1][0]: 
+
+        # If the top of min stack is the same as the top of stack
+        # then we need to decrement the count at the top by 1.
+        if self.min_stack[-1][0] == self.stack[-1]:
             self.min_stack[-1][1] -= 1
 
-        if self.min_stack[-1][1] == 0: 
+        # If the count at the top of min stack is now 0, then remove
+        # that value as we're done with it.
+        if self.min_stack[-1][1] == 0:
             self.min_stack.pop()
-        
+
+        # And like before, pop the top of the main stack.
         self.stack.pop()
-        
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
         return self.min_stack[-1][0]
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
