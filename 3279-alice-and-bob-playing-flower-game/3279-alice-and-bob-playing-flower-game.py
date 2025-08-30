1c8b5a631a9a6a2e912e80adca2317bbaf62ab11
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-08-30 06:05:29 +0530

    Time: 0 ms (100%), Space: 17.7 MB (88.08%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.py
@@ -1,22 +1,23 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        ans = 0
-        oddCOne = 0 
-        evenCOne = 0 
-        oddCTwo = 0
-        evenCTwo = 0
+        return (m * n) // 2 #check editorial for this
+        # ans = 0
+        # oddCOne = 0 
+        # evenCOne = 0 
+        # oddCTwo = 0
+        # evenCTwo = 0
 
-        #Alice would win when x + y is odd :)
-        for i in range(1, n + 1): 
-            if i & 1: 
-                oddCOne += 1
-            else: 
-                evenCOne += 1
+        # #Alice would win when x + y is odd :)
+        # for i in range(1, n + 1): 
+        #     if i & 1: 
+        #         oddCOne += 1
+        #     else:                           
+        #         evenCOne += 1
 
-        for i in range(1, m + 1): 
-            if i & 1: 
-                oddCTwo += 1                        
-            else: 
-                evenCTwo += 1
+        # for i in range(1, m + 1): 
+        #     if i & 1: 
+        #         oddCTwo += 1                        
+        #     else: 
+        #         evenCTwo += 1
 
-        return oddCOne * evenCTwo + oddCTwo * evenCOne       
+        # return oddCOne * evenCTwo + oddCTwo * evenCOne       
