f27cfbf4c0e0d111be6c10bb988cb87c580d108c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-06 02:41:32 +0530

    Time: 0 ms (100%), Space: 17.7 MB (62.65%) - LeetHub

---
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        if num1 == 0: 
+            return 0
+
+        for t in range(0, 61): 
+            s = num1 - (t * num2)
+
+            if s < 0: 
+                continue 
+            if t > s: 
+                continue 
+
+            ones = s.bit_count()
+            if ones <= t: 
+                return t 
+
+        return -1
