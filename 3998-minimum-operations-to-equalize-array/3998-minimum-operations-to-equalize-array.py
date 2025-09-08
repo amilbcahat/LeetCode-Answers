aef98f9e0297326cf21be41fde45f47881539859
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-09 00:12:20 +0530

    Time: 4 ms (7.12%), Space: 17.8 MB (33.33%) - LeetHub

--- 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.py
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.py
@@ -1,7 +1,8 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         for i in range(len(nums) - 1):
+            #if there is a change then bro, convert all
             if nums[i] != nums[i + 1]:
                 return 1 
-        
+        #otherwise not needed 
         return 0
