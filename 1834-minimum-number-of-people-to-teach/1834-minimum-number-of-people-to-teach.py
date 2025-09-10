90d56f8d5576a41b669fd9a7f61600fa340ff913
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-11 05:24:58 +0530

    Time: 66 ms (46.99%), Space: 32.1 MB (16.87%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.py
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.py
@@ -1,29 +1,28 @@
 class Solution:
     def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
-        #preprocess 
+        # preprocess languages array
         user_to_lang = defaultdict(set)
-        for i in range(len(languages)): 
-            user_to_lang[i + 1] = set(languages[i])
+        for i in range(len(languages)):
+            user_to_lang[i+1] = set(languages[i])
 
-        #caculate bad users 
+        # find out which users can't communicate w their friends ("bad")
         bad_users = set()
-        for u1, u2 in friendships: 
+        for u1, u2 in friendships:
             lang1 = user_to_lang[u1]
             lang2 = user_to_lang[u2]
-            if not lang1.intersection(lang2): 
+            if not lang1.intersection(lang2):
                 bad_users.add(u1)
                 bad_users.add(u2)
-
-        if not bad_users: 
+        if not bad_users:
             return 0
 
-        #most spoken language among bad users 
+        # calculate the most spoken language among those bad users
         lang_count = defaultdict(int)
-        max_cnt = 0
-        for user in bad_users:
-            for language in user_to_lang[user]: 
-                lang_count[language] += 1
-                max_cnt = max(max_cnt, lang_count[language])
+        max_lang_count = 0
+        for u in bad_users:
+            for lang in user_to_lang[u]:
+                lang_count[lang] += 1
+                max_lang_count = max(max_lang_count, lang_count[lang])
 
-        #lang count 
-        return len(bad_users) - max_cnt
+        # teach the most spoken language to the users who can't speak it and are "bad"
+        return len(bad_users) - max_lang_count
