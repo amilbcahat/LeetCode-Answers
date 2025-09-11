171776ea9d10d58c2b83d6b8ba4e9e72f637938e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-11 11:09:11 +0530

    Time: 100 ms (37.66%), Space: 19.7 MB (93.29%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -1,14 +1,32 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        arr = list(s)
-        minHeap = []
-        for i,c in enumerate(s): 
-            if c in "aeiouAEIOU": 
-                arr[i] = ""
-                heapq.heappush(minHeap, c)
+        count = [0] * 58
+        s = list(s)
+        for i, c in enumerate(s):
+            if c in 'aeiouAEIOU':
+                count[ord(c) - ord('A')] += 1
+                s[i] = ""
 
-        for i in range(len(s)): 
-            if arr[i] == "":
-                arr[i] = heapq.heappop(minHeap)
+        p = 0
+        for i, c in enumerate(s):
+            if s[i] == "":
+                while p < len(count) and count[p] == 0:
+                    p += 1
 
-        return "".join(arr)
+                s[i] = chr(p + ord('A'))
+                count[p] -= 1
+
+        return "".join(s)
+
+        # arr = list(s)
+        # minHeap = []
+        # for i,c in enumerate(s): 
+        #     if c in "aeiouAEIOU": 
+        #         arr[i] = ""
+        #         heapq.heappush(minHeap, c)
+
+        # for i in range(len(s)): 
+        #     if arr[i] == "":
+        #         arr[i] = heapq.heappop(minHeap)
+
+        # return "".join(arr)
