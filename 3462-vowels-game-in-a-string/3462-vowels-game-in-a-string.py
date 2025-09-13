a4cad51ce8214a9f4f9f579591331843e8ba6615
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-13 14:49:40 +0530

    Time: 31 ms (51.36%), Space: 18.1 MB (79.76%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -1,3 +1,4 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
+        #Editorial - https://leetcode.com/problems/vowels-game-in-a-string/editorial/?envType=daily-question&envId=2025-09-12
         return any(c in 'aeiou' for c in s)
