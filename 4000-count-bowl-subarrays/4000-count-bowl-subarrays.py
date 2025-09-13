20aa762dbbca017e5e6dd04c829ad05933953de5
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-13 17:15:51 +0530

    Time: 285 ms (27.52%), Space: 46.1 MB (5.03%) - LeetHub

---
+++ 4000-count-bowl-subarrays/4000-count-bowl-subarrays.py
@@ -0,0 +1,33 @@
+class Solution:
+    def bowlSubarrays(self, nums: List[int]) -> int:
+        #That is literally a monotonic stack pattern question (missed completely)
+        #This is to be noted
+        left = [-1] * len(nums)
+        right = [len(nums)] * len(nums)
+
+        stack = []
+        for i, n in enumerate(nums): 
+            idx = -1
+            while stack and stack[-1][0] < n:
+                #Pop logic to update and right arrays 
+                val, idx = stack.pop()
+                right[idx] = i
+
+            if stack:
+                left[i] = stack[-1][1]
+
+            stack.append((n, i))
+
+        ans = 0
+        zipped = list(zip(left, right))
+        for i in range(len(zipped)): 
+            leftGreater, rightGreater = zipped[i]
+            if leftGreater != -1 and rightGreater != len(nums): 
+                ans += 1
+
+        return ans
+
+        # stack = []
+        # stack = []
+        # for i in range(len(nums)): 
+
