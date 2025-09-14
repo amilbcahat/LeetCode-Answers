1a5f34266137684a91b017e98873a382f6e829df
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-14 16:51:48 +0530

    Time: 3923 ms (0%), Space: 138.9 MB (0%) - LeetHub

--- 4027-number-of-stable-subsequences/4027-number-of-stable-subsequences.py
+++ 4027-number-of-stable-subsequences/4027-number-of-stable-subsequences.py
@@ -1,32 +1,24 @@
 class Solution:
     def countStableSubsequences(self, nums: List[int]) -> int:
-        # MLE with python
         mod = 10 ** 9 + 7
-        n = len(nums)
-        
-        # Fixed cache array dimensions: [n+1][3][3] to handle -1,0,1 -> 0,1,2
-        # cache = [[[-1] * 3 for _ in range(3)] for _ in range(n + 1)]
+
         @lru_cache
-        def dfs(i, prevPrevParity, prevParity):
-            if i >= len(nums):
+        def dfs(i, prevPrevParity, prevParity): 
+            if i >= len(nums): 
                 return 1
-        
-            
-            # Fixed cache check
-            # if cache[i][pp_idx][p_idx] != -1:
-            #     return cache[i][pp_idx][p_idx]
-            
-            not_include = dfs(i + 1, prevPrevParity, prevParity)
+
             # print(path, prevPrevParity, prevParity)
-            if prevParity != -1 and prevPrevParity != -1:
-                if (prevParity and prevPrevParity and nums[i] % 2 == 0) or (not prevParity and not prevPrevParity and not nums[i] % 2 == 0):
-                    # print(path + [nums[i]], "discarded", prevParity, prevPrevParity, nums[i] % 2 != 0, )
+            not_include = dfs(i + 1, prevPrevParity, prevParity)
+            if prevParity != -1 and prevPrevParity != -1: 
+                if (prevParity and prevPrevParity and nums[i] % 2 == 0) or (not prevParity and not prevPrevParity and not nums[i] % 2 == 0): 
+                    # print(path + [nums[i]], "discarded", prevParity, prevPrevParity, nums[i] % 2 != 0, )             
                     return not_include
-                    
-                    
+                
             res = dfs(i + 1, prevParity, nums[i] % 2 == 0)
-            res = (res + not_include) % mod
-            
-            return res
-        
+            res = (res + (not_include) % mod) % mod
+
+            return res 
+
         return dfs(0, -1, -1) - 1
+
+        
