aefe9c051dab88b11fd871c9e095c8672171c62a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-14 16:49:28 +0530

    Time: 3784 ms (0%), Space: 139.1 MB (0%) - LeetHub

--- 4027-number-of-stable-subsequences/4027-number-of-stable-subsequences.py
+++ 4027-number-of-stable-subsequences/4027-number-of-stable-subsequences.py
@@ -5,31 +5,28 @@ class Solution:
         n = len(nums)
         
         # Fixed cache array dimensions: [n+1][3][3] to handle -1,0,1 -> 0,1,2
-        cache = [[[-1] * 3 for _ in range(3)] for _ in range(n + 1)]
-        
+        # cache = [[[-1] * 3 for _ in range(3)] for _ in range(n + 1)]
+        @lru_cache
         def dfs(i, prevPrevParity, prevParity):
             if i >= len(nums):
                 return 1
-            
-            # Map -1,0,1 to 0,1,2 for array indexing
-            pp_idx = prevPrevParity + 1
-            p_idx = prevParity + 1
+        
             
             # Fixed cache check
-            if cache[i][pp_idx][p_idx] != -1:
-                return cache[i][pp_idx][p_idx]
+            # if cache[i][pp_idx][p_idx] != -1:
+            #     return cache[i][pp_idx][p_idx]
             
+            not_include = dfs(i + 1, prevPrevParity, prevParity)
             # print(path, prevPrevParity, prevParity)
             if prevParity != -1 and prevPrevParity != -1:
                 if (prevParity and prevPrevParity and nums[i] % 2 == 0) or (not prevParity and not prevPrevParity and not nums[i] % 2 == 0):
                     # print(path + [nums[i]], "discarded", prevParity, prevPrevParity, nums[i] % 2 != 0, )
-                    cache[i][pp_idx][p_idx] = dfs(i + 1, prevPrevParity, prevParity)
-                    return cache[i][pp_idx][p_idx]
+                    return not_include
+                    
                     
             res = dfs(i + 1, prevParity, nums[i] % 2 == 0)
-            res = (res + (dfs(i + 1, prevPrevParity, prevParity) % mod)) % mod
+            res = (res + not_include) % mod
             
-            cache[i][pp_idx][p_idx] = res
             return res
         
         return dfs(0, -1, -1) - 1
