9c7f134f4180d81291faf94f125ca70953b38c7e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-21 05:50:03 +0530

    Time: 0 ms (100%), Space: 18.3 MB (26.38%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -44,26 +44,83 @@ class Solution:
         #Approach 1 
         # self.flattenTree(root)
 
-        #Approach 3 
-        # Handle the null scenario
+        #Approach 2 
+         # Handle the null scenario
         if not root:
             return None
 
-        node = root
-        while node:
+        START, END = 1, 2
 
-            # If the node has a left child
-            if node.left:
+        tailNode = None
+        stack = collections.deque([(root, START)])
 
-                # Find the rightmost node
-                rightmost = node.left
-                while rightmost.right:
-                    rightmost = rightmost.right
+        while stack:
 
-                # rewire the connections
-                rightmost.right = node.right
-                node.right = node.left
-                node.left = None
+            currentNode, recursionState = stack.pop()
 
-            # move on to the right side of the tree
-            node = node.right
+            # We reached a leaf node. Record this as a tail
+            # node and move on.
+            if not currentNode.left and not currentNode.right:
+                tailNode = currentNode
+                continue
+
+            # If the node is in the START state, it means we still
+            # haven't processed it's left child yet.
+            if recursionState == START:
+
+                # If the current node has a left child, we add it
+                # to the stack AFTER adding the current node again
+                # to the stack with the END recursion state.
+                if currentNode.left:
+                    stack.append((currentNode, END))
+                    stack.append((currentNode.left, START))
+                elif currentNode.right:
+
+                    # In case the current node didn't have a left child
+                    # we will add it's right child
+                    stack.append((currentNode.right, START))
+            else:
+                # If the current node is in the END recursion state,
+                # that means we did process one of it's children. Left
+                # if it existed, else right.
+                rightNode = currentNode.right
+
+                # If there was a left child, there must have been a leaf
+                # node and so, we would have set the tailNode
+                if tailNode:
+
+                    # Establish the proper connections.
+                    tailNode.right = currentNode.right
+                    currentNode.right = currentNode.left
+                    currentNode.left = None
+                    rightNode = tailNode.right
+
+                if rightNode:
+                    stack.append((rightNode, START))
+
+
+
+
+        #Approach 3 
+        # # Handle the null scenario
+        # if not root:
+        #     return None
+
+        # node = root
+        # while node:
+
+        #     # If the node has a left child
+        #     if node.left:
+
+        #         # Find the rightmost node
+        #         rightmost = node.left
+        #         while rightmost.right:
+        #             rightmost = rightmost.right
+
+        #         # rewire the connections
+        #         rightmost.right = node.right
+        #         node.right = node.left
+        #         node.left = None
+
+        #     # move on to the right side of the tree
+        #     node = node.right
