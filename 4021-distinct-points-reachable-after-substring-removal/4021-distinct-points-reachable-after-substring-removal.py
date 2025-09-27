490312637e74cd884a0f942ddde05b73d2d93911
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-28 01:44:19 +0530

    Time: 424 ms (0%), Space: 26.7 MB (100%) - LeetHub

--- 4021-distinct-points-reachable-after-substring-removal/4021-distinct-points-reachable-after-substring-removal.py
+++ 4021-distinct-points-reachable-after-substring-removal/4021-distinct-points-reachable-after-substring-removal.py
@@ -1,46 +1,7 @@
 class Solution:
     def distinctPoints(self, s: str, k: int) -> int:
 
-        # cache = {}
         got = set()
-        
-        # def dfs(i, r, c): 
-        #     nonlocal newStr
-        #     if i == len(newStr): 
-        #         got.add((r, c))
-        #         return 
-                
-        #     if newStr[i] == "U": 
-        #         dfs(i + 1, r, c + 1)
-
-        #     elif newStr[i] == "D": 
-        #         dfs(i + 1, r, c - 1)
-
-        #     elif newStr[i] == "L": 
-        #         dfs(i + 1, r - 1, c)
-
-        #     elif newStr[i] == "R": 
-        #         dfs(i + 1, r + 1, c)
-        
-        
-        #Generate Strings -> 
-        # n = len(s)
-        # for i in range(n - k + 1): 
-        #     newStr = s[:i] + s[i + k:]
-        #     # print(newStr)
-        #     # dfs(0, 0, 0)
-        #     x = 0
-        #     y = 0
-        #     for c in newStr: 
-        #         if c == "U": 
-        #             y += 1
-        #         elif c == "D": 
-        #             y -= 1
-        #         elif c == "L": 
-        #             x -= 1
-        #         elif c == "R": 
-        #              x += 1
-            # got.add((x, y))
         x = 0
         y = 0 
 
