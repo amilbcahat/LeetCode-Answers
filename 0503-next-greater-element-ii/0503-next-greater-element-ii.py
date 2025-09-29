8831e078482030e4735fb045bc22cde13f613f1c
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-09-30 02:51:16 +0530

    Time: 26 ms (48.66%), Space: 20 MB (6.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        res = [-1] * (len(nums) + len(nums)) #To handle the circular case
+        res = [-1] * (len(nums) + len(nums))
         stack = []
-        cur_max = float("-inf")
-        cur_ind = -1
         for i, n in enumerate(nums + nums): 
-            while stack and stack[-1][0] < n: 
-                popNum, popIndx = stack.pop()
-                res[popIndx] = n 
-            stack.append((n, i))
+            while stack and stack[-1][1] < n: 
+                popIdx, popElem = stack.pop()
+                res[popIdx] = n
+
+            stack.append((i, n))
+
         return res[:len(nums)]
