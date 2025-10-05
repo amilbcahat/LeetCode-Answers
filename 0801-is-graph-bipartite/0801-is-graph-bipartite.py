96a22e2917e37aad97fc7c5d1780162581ceb59b
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-06 04:07:38 +0530

    Time: 4 ms (23.28%), Space: 18.5 MB (54.62%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,20 +1,20 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         visit = defaultdict(int)
-        is_bipartite = True
         def dfs(node, clr): 
-            nonlocal is_bipartite
             visit[node] = clr
-            ans = False
+            ans = True
             for nei in graph[node]: 
                 if nei not in visit: 
-                    dfs(nei, 3 - clr)
+                    ans = dfs(nei, 3 - clr)
                 elif visit[node] == visit[nei]: 
-                    is_bipartite = False
+                    return False
+            return ans
 
 
         for i in range(len(graph)): 
             if i not in visit: 
-                dfs(i, 1)
+                if not dfs(i, 1): 
+                    return False
 
-        return is_bipartite
+        return True
