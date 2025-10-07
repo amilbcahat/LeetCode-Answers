b4178bae2ae4a34a0ee578b5e8dba06016f6ab3e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-08 04:40:49 +0530

    Time: 55 ms (9.58%), Space: 25.4 MB (5.1%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,22 +1,40 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        #Same djishkra like logic to find minimum path 
-        minHeap = [(matrix[0][0], 0 , 0)]
+        #We are going from smallest to greater , thats why this works 
+        minHeap = [(matrix[0][0], 0, 0)]
         n = len(matrix)
         visited = set()
-        while k > 0 and minHeap : 
+        while k > 0 and minHeap: 
             val, i, j = heapq.heappop(minHeap)
 
-            #Either go i + 1 , j or i , j + 1
             if i + 1 < n and (i + 1, j) not in visited: 
-                heapq.heappush(minHeap, (matrix[i + 1][j], i + 1, j))
+                heapq.heappush(minHeap, (matrix[i + 1][j], i  + 1, j))
                 visited.add((i + 1, j))
-            
-            if j + 1 < n and (i , j + 1) not in visited: 
-                heapq.heappush(minHeap, (matrix[i][j + 1], i , j + 1))
-                visited.add((i , j + 1))
 
-            k -= 1 
+            if j + 1 < n and (i, j + 1) not in visited:
+                heapq.heappush(minHeap, (matrix[i][j + 1], i, j + 1))
+                visited.add((i, j + 1))
+
+            k -= 1
 
         return val
+        # Same djishkra like logic to find minimum path 
+        # minHeap = [(matrix[0][0], 0 , 0)]
+        # n = len(matrix)
+        # visited = set()
+        # while k > 0 and minHeap : 
+        #     val, i, j = heapq.heappop(minHeap)
+
+        #     #Either go i + 1 , j or i , j + 1
+        #     if i + 1 < n and (i + 1, j) not in visited: 
+        #         heapq.heappush(minHeap, (matrix[i + 1][j], i + 1, j))
+        #         visited.add((i + 1, j))
+            
+        #     if j + 1 < n and (i , j + 1) not in visited: 
+        #         heapq.heappush(minHeap, (matrix[i][j + 1], i , j + 1))
+        #         visited.add((i , j + 1))
+
+        #     k -= 1 
+
+        # return val
         
