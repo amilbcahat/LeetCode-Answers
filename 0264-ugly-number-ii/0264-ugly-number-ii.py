1db918f7da818ad8e5c93b48dda99ac9dbb11ddc
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-10 04:20:26 +0530

    Time: 80 ms (43.96%), Space: 17.7 MB (95.51%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        minHeap = [1]
+        minHeap  = [1]
         seen = set()
 
-        for _ in range(n): 
+        while minHeap and n : 
             cur_ugly = heapq.heappop(minHeap)
 
-            for cur_p in [2 , 3, 5]: 
-                next_ugly = cur_p * cur_ugly
+            for ugly in [2, 3, 5]: 
+                next_ugly = cur_ugly * ugly 
                 if next_ugly not in seen:
                     heapq.heappush(minHeap, next_ugly)
                     seen.add(next_ugly)
+
+            n -= 1
 
         return cur_ugly
