e50b60b3ac32936bfbe04e0c6e6177d2a08f0461
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-14 02:19:35 +0530

    Time: 9 ms (65%), Space: 21.2 MB (50.79%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,5 +1,8 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        #AlgoZenith framework of BS to find Kth element
+        #Find the check(x) = ((Number of elements <= x) >= k)
+        #Now Binary search for it 
         rows, cols = len(matrix), len(matrix[0])
         def check(x): 
             cntless = 0
@@ -32,9 +35,6 @@ class Solution:
 
         return res
 
-        
-
-
         #We are going from smallest to greater , thats why this works 
         # minHeap = [(matrix[0][0], 0, 0)]
         # n = len(matrix)
@@ -53,23 +53,3 @@ class Solution:
         #     k -= 1
 
         # return val
-        # Same djishkra like logic to find minimum path 
-        # minHeap = [(matrix[0][0], 0 , 0)]
-        # n = len(matrix)
-        # visited = set()
-        # while k > 0 and minHeap : 
-        #     val, i, j = heapq.heappop(minHeap)
-
-        #     #Either go i + 1 , j or i , j + 1
-        #     if i + 1 < n and (i + 1, j) not in visited: 
-        #         heapq.heappush(minHeap, (matrix[i + 1][j], i + 1, j))
-        #         visited.add((i + 1, j))
-            
-        #     if j + 1 < n and (i , j + 1) not in visited: 
-        #         heapq.heappush(minHeap, (matrix[i][j + 1], i , j + 1))
-        #         visited.add((i , j + 1))
-
-        #     k -= 1 
-
-        # return val
-        
