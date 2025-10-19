9b93b322c2d66ca7df7c8a0fcb0c4e91e97807e6
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-19 14:20:39 +0530

    Time: 2630 ms (0%), Space: 18.3 MB (0%) - LeetHub

---
+++ 4045-longest-balanced-subarray-i/4045-longest-balanced-subarray-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            even, odd = 0, 0
+            distinct = defaultdict(int)   
+            for j in range(i, n):
+                if nums[j] % 2 == 0 and nums[j] not in distinct:
+                    even += 1
+                elif nums[j] % 2 and nums[j] not in distinct:
+                    odd += 1
+
+                distinct[nums[j]] += 1
+                # print(nums[i: j + 1], even, odd)
+                if even == odd and j > i:
+                    res = max(res, j - i + 1)
+        # print(nums[i: j + 1])
+
+        return res
