116c37a4c613b075fe8c53aa16551f7c1465d211
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-26 13:18:53 +0530

    Time: 0 ms (100%), Space: 18.3 MB (25.33%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,50 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #Striver Solution -> 
-        #Flatten to 1D Array then solve 
-        rows , cols = len(matrix) , len(matrix[0])
-        l , r = 0 , (rows * cols)- 1
+        rows, cols = len(matrix), len(matrix[0])
+        l = 0 
+        r = (rows * cols) - 1
 
-        while l <= r : 
-            mid = (l + r) // 2 
-
-            #We flatten to 1D array by this logic always 
-            row = mid // cols 
-            col = mid % cols 
-
-            if matrix[row][col] > target : 
+        while l <= r: 
+            mid = (l + r) // 2
+            cr = mid // cols 
+            cc = mid - (cr * cols)
+            if matrix[cr][cc] == target: 
+                return True 
+            elif target <= matrix[cr][cc]: 
                 r = mid - 1
-            elif matrix[row][col] < target : 
+            else: 
                 l = mid + 1
-            else : 
-                return True
-        return False 
 
-        #NeetCode solution -> 
-        # rows , cols = len(matrix) , len(matrix[0])
-
-        # top = 0
-        # bot = rows - 1
-
-        # while top <= bot : 
-        #     row = (top + bot) // 2 
-        #     if target > matrix[row][-1] : 
-        #         top = row + 1
-        #     elif target < matrix[row][0] : 
-        #         bot = row - 1 
-        #     else : 
-        #         break 
-
-        # if top > bot : 
-        #     return False 
-
-        # l , r = 0 , cols - 1
-        # while l <= r : 
-        #     mid = (l + r) // 2 
-        #     if target > matrix[row][mid] : 
-        #         l = mid + 1 
-        #     elif target < matrix[row][mid] : 
-        #         r = mid - 1
-        #     else : 
-        #         return True 
-        # return False 
+        return False
