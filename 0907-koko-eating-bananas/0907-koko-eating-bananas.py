821fbbc3658f1b3fa9ae5c7b4d350b9bb9ad4689
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-26 16:25:28 +0530

    Time: 175 ms (28.58%), Space: 19.3 MB (20.02%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,26 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l , r = 1 , max(piles)
-        #The max value of k will be max pile , since if this pile can be eaten in 1 speed , then others also can be  
-        res = r 
-
-        #Then we just do binary search on this range ! 
-        while l <= r : 
-            k = (l + r ) // 2 
-
+        def check(k): 
             hours = 0 
+            for p in piles: 
+                if k >= p: 
+                    hours += 1
+                else:
+                    hours += math.ceil(p / k)
+            return hours <= h
 
-            for p in piles : 
-                #calculate hours for this k 
-                hours += math.ceil(p / k)
+        l = 1
+        r = max(piles)
+        res = r
 
-            if hours <= h : 
-                res = min(res , k)
-                #if valid , search for even lesser speed 
-                r = k - 1 
-            else : 
-                #if so less speed , that is not even valid , 
-                #Then search for a valid speed
-                l = k + 1 
+        while l <= r: 
+            mid = (l + r) // 2
+            if check(mid): 
+                r = mid - 1
+                res = mid 
+            else: 
+                l = mid + 1
 
-        return res 
+        return res
