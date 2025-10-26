aafa595691f5e6bb9243e24939f4d15fcb7c44e1
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-26 15:30:45 +0530

    Time: 0 ms (100%), Space: 18 MB (66.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        #Just one Binary search
         pivot = 0
 
         def check(arr, mid): 
@@ -17,27 +18,64 @@ class Solution:
             else: 
                 l = mid + 1
 
-        if nums[pivot] <= target <= nums[len(nums) - 1]: 
-            l = pivot 
-            r = len(nums) - 1
-            while l <= r: 
-                mid = (l + r) // 2
-                if nums[mid] == target: 
-                    return mid
-                elif target <= nums[mid]: 
-                    r = mid - 1
-                else: 
-                    l = mid + 1
-        else: 
-            l = 0 
-            r = pivot - 1
-            while l <= r: 
-                mid = (l + r) // 2
-                if nums[mid] == target: 
-                    return mid
-                elif target <= nums[mid]: 
-                    r = mid - 1
-                else: 
-                    l = mid + 1
+        l = 0 
+        r = len(nums) - 1
+
+        print(pivot)
+        while l <= r: 
+            mid = (l + r) // 2
+            shift_idx = (mid + pivot) % len(nums)
+            if nums[shift_idx] == target: 
+                return shift_idx
+            elif nums[shift_idx] < target:
+                l = mid + 1
+            else: 
+                r = mid - 1
 
         return -1
+
+
+        #One solution is simply shifting the indicies
+
+        #Solve based on pivot
+        # pivot = 0
+
+        # def check(arr, mid): 
+        #     if arr[0] > arr[mid]: 
+        #         return True 
+        #     return False 
+
+        # l = 0 
+        # r = len(nums) - 1
+        # while l <= r: 
+        #     mid = (l + r) // 2
+        #     if check(nums, mid): 
+        #         r = mid - 1
+        #         pivot = mid
+        #     else: 
+        #         l = mid + 1
+
+        # if nums[pivot] <= target <= nums[len(nums) - 1]: 
+        #     l = pivot 
+        #     r = len(nums) - 1
+        #     while l <= r: 
+        #         mid = (l + r) // 2
+        #         if nums[mid] == target: 
+        #             return mid
+        #         elif target <= nums[mid]: 
+        #             r = mid - 1
+        #         else: 
+        #             l = mid + 1
+        # else: 
+        #     l = 0 
+        #     r = pivot - 1
+        #     while l <= r: 
+        #         mid = (l + r) // 2
+        #         if nums[mid] == target: 
+        #             return mid
+        #         elif target <= nums[mid]: 
+        #             r = mid - 1
+        #         else: 
+        #             l = mid + 1
+
+        # return -1
