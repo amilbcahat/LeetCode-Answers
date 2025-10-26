40b679816b3dc198d8c4acdf3de1608566b0e54e
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-26 15:35:30 +0530

    Time: 0 ms (100%), Space: 18 MB (91.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,41 +1,69 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         #Just one Binary search
-        pivot = 0
-
-        def check(arr, mid): 
-            if arr[0] > arr[mid]: 
-                return True 
-            return False 
-
-        l = 0 
-        r = len(nums) - 1
-        while l <= r: 
-            mid = (l + r) // 2
-            if check(nums, mid): 
-                r = mid - 1
-                pivot = mid
-            else: 
-                l = mid + 1
-
         l = 0 
         r = len(nums) - 1
 
-        print(pivot)
-        while l <= r: 
+        while l <= r:
             mid = (l + r) // 2
-            shift_idx = (mid + pivot) % len(nums)
-            if nums[shift_idx] == target: 
-                return shift_idx
-            elif nums[shift_idx] < target:
-                l = mid + 1
-            else: 
-                r = mid - 1
+            #Did we find the target ? 
+            if nums[mid] == target: 
+                return mid
+
+            #left is sorted, check if target is present 
+            elif nums[l] <= nums[mid]: 
+                if nums[l] <= target < nums[mid]: 
+                    #target is here find it
+                    r = mid - 1
+                else: 
+                    #not here go to right sub
+                    l = mid + 1
+
+            #if not then right would be sorted
+            else:
+                if nums[mid] < target <= nums[r]: 
+                    #target is here , find now
+                    l = mid + 1
+                else: 
+                    #not here, go to left sub
+                    r = mid - 1
 
         return -1
-
-
+                 
         #One solution is simply shifting the indicies
+        # pivot = 0
+
+        # def check(arr, mid): 
+        #     if arr[0] > arr[mid]: 
+        #         return True 
+        #     return False 
+
+        # l = 0 
+        # r = len(nums) - 1
+        # while l <= r: 
+        #     mid = (l + r) // 2
+        #     if check(nums, mid): 
+        #         r = mid - 1
+        #         pivot = mid
+        #     else: 
+        #         l = mid + 1
+
+        # l = 0 
+        # r = len(nums) - 1
+
+        # print(pivot)
+        # while l <= r: 
+        #     mid = (l + r) // 2
+        #     shift_idx = (mid + pivot) % len(nums)
+        #     if nums[shift_idx] == target: 
+        #         return shift_idx
+        #     elif nums[shift_idx] < target:
+        #         l = mid + 1
+        #     else: 
+        #         r = mid - 1
+
+        # return -1
+
 
         #Solve based on pivot
         # pivot = 0
