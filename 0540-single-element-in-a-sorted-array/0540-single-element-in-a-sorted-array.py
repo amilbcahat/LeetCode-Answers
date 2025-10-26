9373a3c29ce7f5437e18a549280d837fcb8f7e35
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-10-26 17:10:01 +0530

    Time: 0 ms (100%), Space: 24.9 MB (16.46%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,12 +1,13 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        #Answer is always where length is odd of search space
         l = 0 
         r = len(nums) - 1
 
         while l <= r: 
             mid = (l + r) // 2
             if (mid + 1 < len(nums) and nums[mid] != nums[mid + 1]) and (mid - 1 >= 0 and nums[mid] != nums[mid - 1]) or (r - l + 1) == 1: 
-                #Ans is here 
+                #Ans is here, when length is 1 length case odd 
                 return nums[mid]
 
             else: 
@@ -18,7 +19,7 @@ class Solution:
                 if cur_length_start % 2 == 0: 
                     l = mid + 1
                 else: 
-                    #Skip the element, and move to odd search space, leaving this element
+                    #Skip the same element, and move to odd search space, leaving this element
                     mid -= 1
                     r = mid - 1
 
