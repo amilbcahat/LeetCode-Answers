baaf68200bbf09ea4a677d29cf0e860ad8c0d4ff
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-10 20:15:53 +0530

    Time: 172 ms (29.25%), Space: 20.5 MB (29.26%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,33 +1,26 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         #three possibilities 
-        prefix = [[0, 0]]
+        prefix = [0]
         for i in range(len(s)):
             if s[i] == "0":
-                prefix.append([prefix[i][0] + 1, prefix[i][1]])
+                prefix.append(prefix[i] + 1)
             else:
-                prefix.append([prefix[i][0], prefix[i][1] + 1])
+                prefix.append(prefix[i])
 
         n = len(s)
         res = float("inf")
         #0 all 
-
-        res = min(res, n - prefix[n][0])
+        res = min(res, n - prefix[n])
 
         #1 all 
-        res = min(res, prefix[n][0])
+        res = min(res, prefix[n])
 
-        # print(prefix)
         #Some Zeroes then 111... 
         for i in range(1, len(s) + 1): 
-            leftCntOne = prefix[i][1]
-            rightCntZero = prefix[n][0] - prefix[i][0]
+            leftCntOne = i - prefix[i]
+            rightCntZero = prefix[n]- prefix[i]
 
             res = min(res, rightCntZero + leftCntOne)
-            # if prefix[i][0] == i: 
-            #     res = min(res, prefix[n][0] - prefix[i][0])
-            
-            # if prefix[n][1] - prefix[i][1] == n - i: 
-            #     res = min(res, prefix[i][1])
  
         return res
