5e5f627632862d75aede9a893ccdcd64cb834798
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-12 05:52:08 +0530

    Time: 267 ms (16.77%), Space: 32.5 MB (19.88%) - LeetHub

---
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.py
@@ -0,0 +1,24 @@
+class Solution:
+    def secondGreaterElement(self, A: List[int]) -> List[int]:
+        n = len(A)
+        stack = []
+        minHeap = []
+        res = [-1] * n 
+        for i in range(n):
+            while minHeap and minHeap[0][0] < A[i]:
+                elem, targetIdx = heapq.heappop(minHeap)
+                res[targetIdx] = A[i]
+
+            while stack and A[stack[-1]] < A[i]:
+                j = stack.pop()
+                heapq.heappush(minHeap, (A[j], j))
+
+            stack.append(i)
+
+        # print(minHeap)
+        # while minHeap: 
+        #     elem, targetIdx = heapq.heappop(minHeap):
+        #     res[targetIdx] = -1
+
+        return res
+
