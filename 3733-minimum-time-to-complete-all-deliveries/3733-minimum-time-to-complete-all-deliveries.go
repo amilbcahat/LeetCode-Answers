e77cf6ba6bb81d02660347ccaf7ef69a6c8cc017
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-19 20:44:36 +0530

    Time: 0 ms (100%), Space: 4.4 MB (6.93%) - LeetHub

--- 3733-minimum-time-to-complete-all-deliveries/3733-minimum-time-to-complete-all-deliveries.go
+++ 3733-minimum-time-to-complete-all-deliveries/3733-minimum-time-to-complete-all-deliveries.go
@@ -14,15 +14,17 @@ func minimumTime(d []int, r []int) int64 {
     }
 
     check := func(mid int, d []int, r []int) bool{
-        both := mid / lcm(r[0], r[1])
-        first := mid - (mid / r[0])
-        second :=  mid - (mid / r[1])
-        avail := mid - both
+        both := mid / lcm(r[0], r[1]) //Both recharge here 
+        first := mid - (mid / r[0]) //d[1] recharge here 
+        second :=  mid - (mid / r[1]) // d[2] recharge here 
+        avail := mid - both //Left for available
 
         if (first < d[0] || second < d[1]){
+            //Condition of invalid
             return false
         }
 
+        //If availed boxes are more 
         return avail >= (d[0] + d[1])
     }
 
