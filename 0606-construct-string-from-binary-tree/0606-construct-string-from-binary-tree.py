04b2e128f8fa33d18537197f35e5ccc86b801ee8
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-20 23:57:50 +0530

    Time: 3 ms (72.95%), Space: 18.6 MB (100%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
@@ -8,18 +8,19 @@ class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
         res = ""
         def dfs(node):
-            if not node:
+            if not node : 
                 return ""
 
-            result = str(node.val)
-
-            if node.left or node.right:
+            result  = str(node.val)
+            
+            #The checking of node.right here , actually is very important , as it still checks for node.left and if it is not there , it will add ()
+            if node.left or node.right : 
                 result += "(" + dfs(node.left) + ")"
-
-            if node.right:
+            
+            if node.right : 
                 result += "(" + dfs(node.right) + ")"
 
-            return result 
+            return result
 
         return dfs(root)
 
