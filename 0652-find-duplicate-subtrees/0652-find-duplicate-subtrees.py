4a3fd2b8088d3ea18546ea0b40605cdb90151f18
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-21 00:16:26 +0530

    Time: 13 ms (21.88%), Space: 29.5 MB (20.46%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
@@ -8,21 +8,24 @@ class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
         subtrees = defaultdict(list)
 
+        #idea is to map the serialized string value of tree to the root of that serialization order
+        #We can use any traversal here , but we use Preorder here 
         def dfs(node):
-            if not node:
+            if not node: 
+            #Null value are actually whats important , to distinguish two subtress having same nodes , but different 
+            #pathways !
                 return "null"
+            s = ",".join([str(node.val) , dfs(node.left) , dfs(node.right)])
 
-            s = ",".join([ str(node.val) , dfs(node.left), dfs(node.right)])
-
+            #The first duplicate entry is stored in the result 
             if len(subtrees[s]) == 1:
                 res.append(node)
 
+            #We map the serialized order to its node 
             subtrees[s].append(node)
-
-            return s
+            #Return the so far , serialized order 
+            return s 
 
         res = []
         dfs(root)
-
-        return res
-            
+        return res 
