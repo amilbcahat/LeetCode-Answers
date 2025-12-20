524a7766ec351de30f16df568375641b5081943a
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-20 17:43:49 +0530

    Time: 41 ms (96.44%), Space: 18.7 MB (99.46%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,20 +10,8 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        # cur, nxt = root, root.left if root else None 
+        #nxt here means nxt level to start from!
 
-        # while cur and nxt: 
-        #     cur.left.next = cur.right 
-
-        #     if cur.next: 
-        #         cur.right.next = cur.next.left
-
-        #     cur = cur.next 
-        #     if not cur:
-        #         cur = nxt
-        #         nxt = cur.left
-        
-        # return root
         cur, nxt = root, root.left if root else None 
         while cur and nxt: 
             cur.left.next = cur.right
