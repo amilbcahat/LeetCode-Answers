37f02f06756de5d379b9ae01b2a0f5d605b3bd62
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-20 22:21:31 +0530

    Time: 0 ms (100%), Space: 17.5 MB (98.89%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.py
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.py
@@ -1,29 +1,15 @@
 class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
-        # Initialize the number of available slots for nodes in the binary tree.
-        availableSlots = 1
+        slots = 1 
+        preorder = preorder.split(',')
 
-        # Get the length of the input string.
-        length = len(preorder)
+        for node in preorder: 
+            slots -= 1
+            if slots < 0: 
+                return False 
+            if node != '#':
+                slots += 2
+            
 
-        # Traverse through each character in the input string.
-        for i in range(length):
-            # If a comma is encountered, a new node has been processed.
-            if preorder[i] == ',':
-                # Decrease the available slots by 1 as the node consumes one slot.
-                availableSlots -= 1
 
-                # If no slots are available, the serialization is invalid.
-                if availableSlots < 0:
-                    return False
-
-                # If the previous character is not '#', it represents a non-null node,
-                # which creates two additional slots for its children.
-                if preorder[i - 1] != '#':
-                    availableSlots += 2
-
-        # Process the last node in the input string.
-        availableSlots = availableSlots - 1 if preorder[length - 1] == '#' else availableSlots + 1
-
-        # The serialization is valid if all slots are exactly filled.
-        return availableSlots == 0
+        return slots == 0
