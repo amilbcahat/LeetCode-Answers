98edd0f61ebc7da8b0e1f1cd0a903e3f325e9172
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-21 14:59:31 +0530

    Time: 61 ms (33.51%), Space: 26.2 MB (61.74%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -5,28 +5,35 @@ class Solution:
         #BFS from outside to inside !!!
         if n == 1: #or -> not edges condition
             return [0]
-        
+
         adj = defaultdict(list)
-        for src, dst in edges: 
+
+        for src, dst in edges:
             adj[src].append(dst)
             adj[dst].append(src)
 
-        edge_cnt = {}
         leaves = deque()
-        for src, neighbours in adj.items():
-            if len(neighbours) == 1: 
-                leaves.append(src)
-            edge_cnt[src] = len(neighbours)
+
+        edge_cnt = {}
+        
+        for node in adj:
+            nei = adj[node]
+            if len(nei) == 1:
+                leaves.append(node)
+            edge_cnt[node] = len(nei)
 
         while leaves: 
-            if n <= 2: #Intuition , that , when reducing the leaves, if less/equal than 2 left then thats leaf nodes !!
+            if n <= 2: 
                 return list(leaves)
 
             for i in range(len(leaves)):
                 node = leaves.popleft()
-                n -= 1
+                n -= 1 
                 for nei in adj[node]:
-                    edge_cnt[nei] -= 1 
-                    if edge_cnt[nei] == 1: #condition for leaf node  
-                        #Is a leaf node 
+                    edge_cnt[nei] -= 1
+                    if edge_cnt[nei] == 1:
                         leaves.append(nei)
+
+        
+
+            
