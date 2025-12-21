aeaec89cdd313d8a0bb5085c30f25ecaae0c49d0
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-21 13:39:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for src, pre in prerequisites: 
+            adj[src].append(pre)
+
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return False 
+
+            if not adj[node]: 
+                return True 
+
+            visited.add(node)
+
+            for nei in adj[node]:
+                if not dfs(pre):
+                    return False 
+
+            visited.remove(node)
+
+            adj[node] = []
+            return True 
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False 
+
+        return True 
+
+            
