7090395083338ba97c785d623cb047a673542c7f
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-21 20:48:39 +0530

    Time: 713 ms (52.94%), Space: 70.5 MB (98.74%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]]) -> int:
+        #Notes to myself, 
+        #Tried with dfs in contest , when needed to use djiskhtra
+        #Messed up in node cost as well 
+        adj = defaultdict(list)
+        for src, dst, cost in edges: 
+            adj[src].append((dst, cost))
+            adj[dst].append((src, cost * 2))
+
+        visit = set()
+
+        minHeap = [(0 , 0)]
+        while minHeap:
+            nodeCost, node = heapq.heappop(minHeap) 
+            if node == n - 1: 
+                return nodeCost
+
+            if node in visit: 
+                continue
+
+            visit.add(node)
+            for nei, c in adj[node]: 
+                #dont reverse 
+                heapq.heappush(minHeap, (c + nodeCost, nei))
+
+        return -1
+                
