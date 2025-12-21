c42f8616baede1fca0994d578726f8de11a000c6
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-21 17:49:25 +0530

    Time: 3 ms (93.61%), Space: 20.4 MB (9.95%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -3,40 +3,55 @@ class Solution:
         # Editorial please check always
         # This will help in understanding that - https://math.stackexchange.com/a/3961816
         # Backtracking
-        n = len(graph)
-        res = []
-        def dfs(node, path):
-            nonlocal res 
-            if node == n - 1: 
-                res.append(list(path))
-                return
+        # n = len(graph)
+        # res = []
+        # def dfs(node, path):
+        #     nonlocal res 
+        #     if node == n - 1: 
+        #         res.append(list(path))
+        #         return
 
-            for nei in graph[node]: 
-                path.append(nei)
-                dfs(nei, path) 
-                path.pop()
+        #     for nei in graph[node]: 
+        #         path.append(nei)
+        #         dfs(nei, path) 
+        #         path.pop()
 
-        dfs(0, [0])
+        # dfs(0, [0])
 
-        return res
+        # return res
 
         #Top Down Approach 
         # allPathsToTarget(currNode)={currNode+allPathsToTarget(nextNode)}
-        # target = len(graph) - 1
+        target = len(graph) - 1
 
-        # @lru_cache(maxsize=None)
-        # def all_paths_to_target(node): 
-        #     if node == target: 
-        #         return [[target]]
+        @lru_cache(maxsize=None)
+        def all_paths_to_target(node): 
+            if node == target: 
+                return [[target]]
 
-        #     path_to_target_from_this_node = []
-        #     for nei in graph[node]: 
-        #         for path in all_paths_to_target(nei): 
-        #             path_to_target_from_this_node.append([node] + path)
+            path_to_target_from_this_node = []
+            for nei in graph[node]: 
+                for path in all_paths_to_target(nei): 
+                    path_to_target_from_this_node.append([node] + path)
             
-        #     return path_to_target_from_this_node
+            return path_to_target_from_this_node
 
-        # return all_paths_to_target(0)
+        return all_paths_to_target(0)
+
+        # n = len(graph) - 1
+
+        # def dfs(node):
+        #     if node == n: 
+        #         return [[n]]
+
+        #     paths = []
+        #     for nei in graph[node]:
+        #         for path in dfs(nei):
+        #             paths.append([node] + path)
+
+        #     return paths
+        
+        # return dfs(0)
 
 #         The Key Insight: How Many Paths Can Exist?
 # First, let's understand why we get 2^N paths in the worst case:
