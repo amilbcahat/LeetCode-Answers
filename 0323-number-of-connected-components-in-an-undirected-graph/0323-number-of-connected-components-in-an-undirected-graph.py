d84ea884198329e969c7e01f3995beae91a3a165
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-21 15:21:08 +0530

    Time: 7 ms (51.04%), Space: 19 MB (71.71%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,39 +1,28 @@
-class UnionFind: 
-    def __init__(self, n):
-        self.par = {}
-        self.rank = {}
-        self.comp = n
-
-        for i in range(n): 
-            self.par[i] = i
-            self.rank[i] = 0 
-
-    def find(self, n): 
-        p = self.par[n]
-        while p != self.par[p]:
-            self.par[p] = self.par[self.par[p]]
-            p = self.par[p]
-        return p 
-    
-    def union(self, n1, n2): 
-        p1, p2 = self.find(n1), self.find(n2)
-        if p1 == p2: 
-            return False 
-
-        if self.rank[p1] < self.rank[p2]: 
-            self.par[p1] = p2
-        elif self.rank[p2] < self.rank[p1]:
-            self.par[p2] = p1
-        else:
-            self.par[p1] = p2
-            self.rank[p2] += 2
-        self.comp -= 1
-        return True
-
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
-        for src, dst in edges: 
-            uf.union(src, dst)
+        adj = defaultdict(list)
 
-        return uf.comp
+        for src, dst in edges: 
+            adj[src].append(dst)
+            adj[dst].append(src)
+
+
+        visit = set()
+
+        def dfs(node):
+            visit.add(node)
+            # comp_map[node] = comp_no
+
+            for nei in adj[node]:
+                if nei not in visit:
+                    dfs(nei)
+
+
+        comp_no = 0
+        for i in range(n):
+            if i not in visit: 
+                comp_no += 1
+                dfs(i)
+
+        return comp_no
+                
