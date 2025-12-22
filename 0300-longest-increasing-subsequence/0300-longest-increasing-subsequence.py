0fed1df471e2334c8f2a85f1e8e457179b1c1e51
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-22 12:11:27 +0530

    Time: 1827 ms (20.01%), Space: 17.7 MB (97.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,18 +1,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-
+        #Base Case 
+        #For [1,2,4,3]
+        #LIS[3] =1 Base case 
+        #LIS[2] = max(1, LIS[3]) X -> 1 
+        #LIS[1] = max(1, LIST[2]+1 , LIS[3]+1) -> 2 
+        #LIS[0] = max(1 , 1+1 , 1+1 ,1+2) -> 3  
         LIS = [1] * len(nums)
-
-        for i in range(len(nums) - 1, -1 , -1):
-            for j in range(i + 1 , len(nums)):
-                if nums[j] > nums[i] : 
-                    LIS[i] = max(LIS[i] , 1 + LIS[j])
-                    #Check with prev element (who are bigger than current element)
-                    #If found then we take max of current (to compare all conditions) and 1 + prev elem subproblem
-                    #1 + is done to account for the added length and updated value !
-                    
-
-        
-        #LIS[0] is not taken because , it is possible tha nums[i] < nums[j] does not hold everytime , so we mess up the 
-        #solution , and solution can be anywhere in the array !
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[j] > nums[i]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
         return max(LIS)
