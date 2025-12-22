ca08a583698fc3d7458d114ea5221ebf3938f772
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-22 12:05:19 +0530

    Time: 3 ms (69.42%), Space: 17.3 MB (98.89%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,16 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
+        dp = [False] * (len(s)+1)
         #considering that we got a valid one at last position !
         dp[len(s)] = True 
 
-        #dp[0] tells that if dp[1:] of the array is able to segment or not 
-        for i in range(len(s) - 1 , -1 , -1) : 
+        #We use dp over here and to loop to basically ascertain ending, rest of the things can be understood as hopping ans
+        for i in range(len(s)-1,-1,-1):
             for w in wordDict : 
-                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w : 
+                if i + len(w) <= len(s) and s[i: i + len(w)] == w:
                     dp[i] = dp[i + len(w)]
-                if dp[i] : 
-                    break 
-
+                if dp[i]:
+                    break
         #If all words are in WordDict then it will reach True first position as well 
-        return dp[0]        
+        return dp[0]
