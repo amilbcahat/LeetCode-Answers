76829aba84911cf87c0df0e88d3795e9f0f0db1d
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-22 12:55:22 +0530

    Time: 49 ms (78.01%), Space: 19.1 MB (90.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,22 +3,24 @@ class Solution:
         cache = {}
 
         def dfs(i, j):
-            if i == len(word1):
-                return len(word2) - j  # Insert remaining
-            if j == len(word2):
-                return len(word1) - i  # Delete remaining
-            
+            if i == len(word1): 
+                return len(word2) - j 
+
+            if j == len(word2): 
+                return len(word1) - i
+
             if (i, j) in cache:
                 return cache[(i, j)]
-            
+
             if word1[i] == word2[j]:
-                cache[(i, j)] = dfs(i + 1, j + 1)
+                cache[(i, j)] = dfs(i + 1, j + 1) 
             else:
                 insert = dfs(i, j + 1)
                 delete = dfs(i + 1, j)
                 replace = dfs(i + 1, j + 1)
+
                 cache[(i, j)] = 1 + min(insert, delete, replace)
-            
+
             return cache[(i, j)]
         
         return dfs(0, 0)
