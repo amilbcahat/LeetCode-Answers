a87ba8ecba1ee5fde0da8ee005ffd5ffcfdb467f
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-24 02:03:25 +0530

    Time: 8 ms (59.23%), Space: 17.7 MB (95.73%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -1,18 +1,19 @@
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
         pairs.sort()
+        #Same answer as Minimum number of arrows to burst balloons 
 
         #Non overlapping intervals
-        res = 0 
+        res = len(pairs)
         prev = pairs[0]
         for i in range(1, len(pairs)): 
             cur = pairs[i]
             if cur[0] <= prev[1]: 
-                res += 1
+                res -= 1
                 prev = [
                 cur[0] #prev[0] works fine too here
                 , min(prev[1], cur[1])
                 ]
             else:
                 prev = cur 
-        return len(pairs) - res
+        return res
