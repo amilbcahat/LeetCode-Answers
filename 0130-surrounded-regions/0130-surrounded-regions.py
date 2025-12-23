268bbb692081e42b0897924d5431cc5c0335020f
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-23 11:24:00 +0530

    Time: 12 ms (21.85%), Space: 20.9 MB (98.13%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,23 +1,25 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        rows , cols = len(board) , len(board[0])
-        visit = set() 
-        def dfs(r , c) : 
-            if (r , c) in visit or r not in range(rows) or c not in range(cols) or board[r][c] == "X" : 
+        #First we did DFS specially for boundary "O" and added adjacent "O" in visit set , then traversed whole matrix again and if there is a "O" , that is not in visit ,then it is flipped ! 
+        rows, cols = len(board), len(board[0])
+        visit = set()
+        def dfs(r, c):
+            if r not in range(rows) or c not in range(cols) or board[r][c] == "X" or (r, c) in visit:
                 return 
 
-            visit.add((r  , c))
+            visit.add((r, c))
             dfs(r + 1, c)
             dfs(r - 1, c)
-            dfs(r , c+ 1)
-            dfs(r , c - 1)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
 
-        for r in range(rows) : 
-            for c in range(cols) : 
-                if r == 0 or c == 0 or c == cols - 1 or r == rows - 1 : 
-                    dfs(r , c)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 or c == 0 or c == cols - 1 or r == rows - 1:
+                    dfs(r, c)
 
         for r in range(rows):
-            for c in range(cols) : 
-                if (r , c) not in visit and board[r][c] == "O" : 
+            for c in range(cols):
+                if (r, c) not in visit and board[r][c] == "O":
                     board[r][c] = "X"
