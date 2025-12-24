2a5c1c11a98e4cec4955f01b58671d4337a44640
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-24 10:32:27 +0530

    Time: 0 ms (100%), Space: 17.2 MB (98.77%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,26 +1,25 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftMin = 0 
-        leftMax = 0
-        ast = 0
-        for c in s: 
-            if c == "(": 
-                leftMin += 1
-                leftMax += 1
-            elif c == ")":
-                leftMin -= 1
-                leftMax -= 1 
-            else:
-                leftMin -= 1
-                leftMax += 1
-                ast += 1
+        left = []
+        ast = []
 
-            if leftMax < 0: 
+        for i,char in enumerate(s):
+            if char == '(':
+                left.append(i)
+            elif char == '*':
+                ast.append(i)
+                # The next three conditions are when char == ")"
+            elif left : # Match left paren with right paren
+                left.pop()
+            elif ast : # Match asterix with right paren
+                ast.pop()
+            else : # Can't match this right paren with anything
                 return False 
-
-            if leftMin < 0: 
-                leftMin = 0
-
-        return leftMin == 0         
-
-            
+# Found matches for all right parens, now match any leftover left parens
+		# Ensure any asterix used as a right paren comes after the left paren we are matching
+        
+        while left and ast and left[-1] < ast[-1]:
+            left.pop()
+            ast.pop()
+		# If there's any leftover left parens, then we couldn't match those, so False
+        return not left 
