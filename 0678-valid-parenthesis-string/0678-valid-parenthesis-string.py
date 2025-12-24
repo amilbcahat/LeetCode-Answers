750ec3c51cea534f04856c8335b11c2fafdbdaef
Amol Verma ( Dingus ) <68186313+amilbcahat@users.noreply.github.com>
2025-12-24 10:06:00 +0530

    Time: 0 ms (100%), Space: 17.5 MB (90.37%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin = 0 
+        leftMax = 0
+        ast = 0
+        for c in s: 
+            if c == "(": 
+                leftMin += 1
+                leftMax += 1
+            elif c == ")":
+                leftMin -= 1
+                leftMax -= 1 
+            else:
+                leftMin -= 1
+                leftMax += 1
+                ast += 1
+
+            if leftMax < 0: 
+                return False 
+
+            if leftMin < 0: 
+                leftMin = 0
+
+        return leftMin == 0         
+
+            
